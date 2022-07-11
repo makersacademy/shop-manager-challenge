1f029ba8eb5333989e33b46401977c9081fab1f1
delphine <delphine.mubiligi@gmail.com>
2022-07-11 11:18:06 +0100

    refactoring create method for application class

--- app.rb
+++ app.rb
@@ -33,36 +33,31 @@ class Application
   end
 
   def create_item
-    @io.puts "Enter the id of the item: "
-    id = @io.gets.chomp
-    @io.puts "Enter the name of the item: "
-    name = @io.gets.chomp
-    @io.puts "Enter the price of the item: "
-    price = @io.gets.chomp.to_f
-    @io.puts "Enter quantity: "
-    quantity = @io.gets.chomp.to_i
-
     new_item = Item.new
-    new_item.id = id
-    new_item.name = name
-    new_item.price = price
-    new_item.quantity = quantity
+    @io.puts "Enter the id of the item: "
+    new_item.id = @io.gets.chomp
+    @io.puts "Enter the name of the item: "
+    new_item.name = @io.gets.chomp
+    @io.puts "Enter the price of the item: "
+    new_item.price = @io.gets.chomp.to_f
+    @io.puts "Enter quantity: "
+    new_item.quantity = @io.gets.chomp.to_i
+    
     @item_repository.create(new_item)
+    @io.puts "Your item was successfully added!"
   end
 
   def create_order
-    @io.puts "Enter the id of the order: "
-    id = @io.gets.chomp
-    @io.puts "Enter the name of the customer: "
-    customer = @io.gets.chomp
-    @io.puts "Enter the date of the order: "
-    date = @io.gets.chomp
-
     new_order = Order.new
-    new_order.id = id
-    new_order.customer = customer
-    new_order.date = date
+    @io.puts "Enter the id of the order: "
+    new_order.id = @io.gets.chomp
+    @io.puts "Enter the name of the customer: "
+    new_order.customer = @io.gets.chomp
+    @io.puts "Enter the date of the order: "
+    new_order.date = @io.gets.chomp
+
     @order_repository.create(new_order)
+    @io.puts "Your order was successfully added!"
   end
 
   def run
@@ -82,14 +77,10 @@ class Application
         all_items
       when "2"
         create_item
-        @io.puts "Your item was successfully added!"
-        all_items
       when "3"
         all_orders
       when "4"
         create_order
-        @io.puts "Your order was successfully added!"
-        all_orders
     end
     # "Runs" the terminal application
     # so it can ask the user to enter some input
