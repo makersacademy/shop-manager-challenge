f8b0839d137aa4cef23a763c306458132bcb0197
Yichao <yq2550@gmail.com>
2022-09-10 18:27:55 +0100

    initialize class OrderRepository

---
+++ lib/order_repository.rb
@@ -0,0 +1,26 @@
+require_relative 'order'
+
+class OrderRepository
+  def all
+    sql = 'SELECT * FROM items'
+    result = DatabaseConnection.exec_params(sql, [])
+    
+    items = []
+    result.each do |record|
+      item = Item.new
+      item.id = record['id'].to_i
+      item.name = record['name']
+      item.unit_price = record['unit_price'].to_i
+      item.quantity = record['quantity'].to_i
+      items << item
+    end
+    return items
+  end
+
+  def create(item)
+    sql = 'INSERT INTO items (name, unit_price, quantity) VALUES ($1, $2, $3)'
+    params = [item.name, item.unit_price, item.quantity]
+    result = DatabaseConnection.exec_params(sql, params)
+    return nil
+  end
+end
---
+++ spec/order_repository_spec.rb
@@ -0,0 +1,34 @@
+require 'order_repository'
+
+RSpec.describe OrderRepository do
+  def reset_table
+    seed_sql = File.read('spec/seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'shop' })
+    connection.exec(seed_sql)
+  end
+
+  before(:each) do 
+    reset_table
+  end
+
+  it "returns an array of all shop items" do
+    repo = ItemRepository.new
+    items = repo.all
+    expect(items.count).to eq 5
+    expect(items[0].name).to eq 'voile'
+    expect(items[0].unit_price).to eq 13
+    expect(items[0].quantity).to eq 260
+  end
+
+  it "creates a shop item" do
+    repo = ItemRepository.new
+    new_item = Item.new
+    new_item.name = 'coffee pods'
+    new_item.unit_price = '5'
+    new_item.quantity = '10'
+    repo.create(new_item)
+    items = repo.all
+    expect(items.count).to eq 6
+    expect(items.last.name).to eq 'coffee pods'
+  end
+end
