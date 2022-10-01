597ce9dd398cbb48e9d16759ec145cb69dc0d6f0
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-10-01 11:26:01 +0100

    Project setup and create tables and database

---
+++ spec/items_orders.sql
@@ -0,0 +1,22 @@
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY,
+  name text,
+  unit_price int,
+  quantity int
+);
+
+-- Create the second table.
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  customer_name text,
+  date date
+);
+
+-- Create the join table.
+CREATE TABLE items_orders (
+  item_id int,
+  order_id int,
+  constraint fk_item foreign key(item_id) references items(id) on delete cascade,
+  constraint fk_order foreign key(order_id) references orders(id) on delete cascade,
+  PRIMARY KEY (item_id, order_id)
+);
