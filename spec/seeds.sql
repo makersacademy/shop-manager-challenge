4b7ea8245fabf064640e6cfba53feec824bd2165
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-10-01 11:55:05 +0100

    Modify tables and seeds, add remaining files

--- spec/seeds.sql
+++ spec/seeds.sql
@@ -1,4 +1,4 @@
-TRUNCATE TABLE items_orders RESTART IDENTITY;
+TRUNCATE TABLE items, orders RESTART IDENTITY;
 
 INSERT INTO items (name, unit_price, quantity) VALUES ( 'Royal Canin kitten food', '5', '20');
 INSERT INTO items (name, unit_price, quantity) VALUES ( 'Organic tomatoes', '3', '10');
@@ -6,15 +6,11 @@ INSERT INTO items (name, unit_price, quantity) VALUES ( 'Celery', '2', '7');
 INSERT INTO items (name, unit_price, quantity) VALUES ( 'Scottish salmon', '6', '9');
 INSERT INTO items (name, unit_price, quantity) VALUES ( 'Baby leaves', '3', '15');
 
-INSERT INTO orders (customer_name, date) VALUES ('Alice', '2022-09-15');
-INSERT INTO orders (customer_name, date) VALUES ('Pedro', '2022-08-30');
-INSERT INTO orders (customer_name, date) VALUES ('Hannah', '2022-10-01');
+INSERT INTO orders (customer_name, date, item_id) VALUES ('Alice', '2022-09-15', '1');
+INSERT INTO orders (customer_name, date, item_id) VALUES ('Pedro', '2022-08-30', '2');
+INSERT INTO orders (customer_name, date, item_id) VALUES ('Hannah', '2022-10-01', '5');
 
-INSERT INTO items_orders (item_id, order_id) VALUES (1, 1);
-INSERT INTO items_orders (item_id, order_id) VALUES (2, 1);
-INSERT INTO items_orders (item_id, order_id) VALUES (3, 2);
-INSERT INTO items_orders (item_id, order_id) VALUES (4, 2);
-INSERT INTO items_orders (item_id, order_id) VALUES (5, 3);
+
 
 
 
