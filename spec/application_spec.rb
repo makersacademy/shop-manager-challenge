c2eecea03e23f64de022a46e60fb829f1ebd125d
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-10-02 09:20:40 +0100

    Add test for printing list of items

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -28,6 +28,28 @@ RSpec.describe Application do
         app = Application.new(io)
         app.run
     end
+
+    it "prints a list of items if user input is 1" do
+        io = double :io
+        app = Application.new(io)
+
+        expect(io).to receive(:puts).with("Welcome to the shop management program!")
+        expect(io).to receive(:puts).with("")
+        expect(io).to receive(:puts).with("What do you want to do?")
+        expect(io).to receive(:puts).with("  1 = list all shop items")
+        expect(io).to receive(:puts).with("  2 = create a new item")
+        expect(io).to receive(:puts).with("  3 = list all orders")
+        expect(io).to receive(:puts).with("  4 = create a new order")
+        expect(io).to receive(:gets).and_return("1")
+        expect(io).to receive(:puts).with("Here's a list of all shop items:")
+        expect(io).to receive(:puts).with("1 Royal Canin kitten food - Unit price: 5 - Quantity: 20")
+        expect(io).to receive(:puts).with("2 Organic tomatoes - Unit price: 3 - Quantity: 10")
+        expect(io).to receive(:puts).with("3 Celery - Unit price: 2 - Quantity: 7")
+        expect(io).to receive(:puts).with("4 Scottish salmon - Unit price: 6 - Quantity: 9")
+        expect(io).to receive(:puts).with("5 Baby leaves - Unit price: 3 - Quantity: 15")
+
+        app.run
+    end
 end
 
 
