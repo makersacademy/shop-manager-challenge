f795bbefd2efe71ea3a19a9a02dbc544a58a111c
adonl <adnlwly@gmail.com>
2022-10-28 16:13:07 +0100

    add app with first menu option and test

---
+++ app.rb
@@ -0,0 +1,58 @@
+require_relative 'lib/database_connection'
+require_relative 'lib/item_repository'
+require_relative 'lib/order_repository'
+
+class Application
+
+  def initialize(database_name, terminal, item_repository, order_repository)
+    DatabaseConnection.connect(database_name)
+    @terminal = terminal
+    @item_repository = item_repository
+    @order_repository = order_repository
+  end
+
+  def run
+  # Runs application and loads interface menu
+    @terminal.puts "Welcome to Shop Manager!"
+    @terminal.puts "Pick an option (input number):"
+    @terminal.puts "1 - List items in stock"
+    @terminal.puts "2 - Add new item to stock"
+    @terminal.puts "3 - List all orders on record"
+    @terminal.puts "4 - Add new order to record"
+    input = @terminal.gets.chomp
+
+    case input
+    when '1'
+      result = @item_repository.all
+      result.each do |record| 
+        @terminal.puts "ITEM: #{record.name} - PRICE: #{record.price} - STOCK: #{record.quantity} units"
+      end
+    end
+  end
+
+  def list_item_stock
+  # Prints list of items in stock
+  end
+
+  def list_orders
+  # Prints list of orders on record
+  end
+
+  def create_item
+  # Adds new item to stock
+  end
+
+  def create_order
+  # Adds new order to record
+  end
+end
+
+if __FILE__ == $0
+  app = Application.new(
+    'shop_manager',
+    Kernel,
+    ItemRepository.new,
+    OrderRepository.new
+  )
+  app.run
+end
---
+++ spec/app_spec.rb
@@ -0,0 +1,46 @@
+require_relative '../app'
+
+def reset_shop_table
+  seed_sql = File.read('spec/seeds_shop.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_manager_test' })
+  connection.exec(seed_sql)
+end
+ 
+describe Application do
+  before(:each) do 
+    reset_shop_table
+  end
+
+  it 'prints out all items in the shop' do
+    terminal = double(:terminal)
+    expect(terminal).to receive(:puts).with("Welcome to Shop Manager!").ordered
+    expect(terminal).to receive(:puts).with("Pick an option (input number):").ordered
+    expect(terminal).to receive(:puts).with("1 - List items in stock").ordered
+    expect(terminal).to receive(:puts).with("2 - Add new item to stock").ordered
+    expect(terminal).to receive(:puts).with("3 - List all orders on record").ordered
+    expect(terminal).to receive(:puts).with("4 - Add new order to record").ordered
+    expect(terminal).to receive(:gets).and_return("1").ordered
+    expect(terminal).to receive(:puts).with('ITEM: Socks - PRICE: $2.50 - STOCK: 100 units').ordered
+    expect(terminal).to receive(:puts).with('ITEM: Trousers - PRICE: $11.00 - STOCK: 75 units').ordered
+    expect(terminal).to receive(:puts).with('ITEM: Shirt - PRICE: $12.00 - STOCK: 60 units').ordered
+    expect(terminal).to receive(:puts).with('ITEM: Shoes - PRICE: $30.00 - STOCK: 50 units').ordered
+    expect(terminal).to receive(:puts).with('ITEM: Skirt - PRICE: $7.50 - STOCK: 65 units').ordered
+    expect(terminal).to receive(:puts).with('ITEM: Hat - PRICE: $5.50 - STOCK: 90 units').ordered
+    expect(terminal).to receive(:puts).with('ITEM: Scarf - PRICE: $3.50 - STOCK: 120 units').ordered
+    expect(terminal).to receive(:puts).with('ITEM: Dress - PRICE: $12.50 - STOCK: 70 units').ordered
+    items = ItemRepository.new
+    orders = OrderRepository.new
+    app = Application.new('shop_manager_test', terminal, items, orders)
+    app.run
+  end
+
+  # it 'creates a new item' do
+  # end
+
+  # it 'lists all orders on shop record' do
+  # end
+
+  # it 'creates a new order' do
+  # end
+
+end
