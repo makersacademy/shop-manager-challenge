c40852b6da77678ae16080de631697feb3decc62
Nicola Hearn <nicolahearn@mastude5h3vq6l4.home>
2022-11-27 09:50:14 +0000

    passes 3rd test - creates a new item if menu choice is 2

--- lib/app.rb
+++ lib/app.rb
@@ -2,6 +2,7 @@
 
 require_relative 'database_connection'
 require_relative 'item_repository'
+require_relative 'item'
 
 # We need to give the database name to the method `connect`.
 DatabaseConnection.connect('shop_test')
@@ -24,16 +25,21 @@ class Menu
     end
   end
 
-  # def menu_1
-  #   @t_menu1.puts "Here's a list of shop items"
-  #   results = ItemRepository.new
-  #   results.all.each do |record|
-  #     puts "#{record.id} - #{record.item_name} - #{record.unit_price} - #{record.quantity}"
-  #   end
-  # end
+  def menu_2
+    @terminal.puts "Let's create a new item!\nEnter a name for this item"
+    results = ItemRepository.new
+    item = Item.new
+    item.item_name = @terminal.gets
+    @terminal.puts "Enter a price for this item"
+    item.unit_price = @terminal.gets
+    @terminal.puts "Enter a quantity for this item"
+    item.quantity = @terminal.gets
+
+    results.create(item)
+  end
 
   def run_menu
-    @terminal.puts "Welcome to the shop management program!\n\nWhat would you like to do? (type a number)\n1 = list all shop items\n2 = create a new item\n3 = list all orders\n4 = create a new order"
+   @terminal.puts "Welcome to the shop management program!\n\nWhat would you like to do? (type a number)\n1 = list all shop items\n2 = create a new item\n3 = list all orders\n4 = create a new order"
     
     menu_choice = @terminal.gets
   
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,7 +1,24 @@
 require 'app'
 require 'item_repository'
+require 'item'
 
 describe Menu do
+  def reset_items_table
+    seed_sql = File.read('spec/seeds_items.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_test' })
+    connection.exec(seed_sql)
+  end
+
+  def reset_orders_table
+    seed_sql = File.read('spec/seeds_orders.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_test' })
+    connection.exec(seed_sql)
+  end
+
+  before(:each) do
+    reset_items_table
+    reset_orders_table
+  end
   describe "run_menu" do
     it "prints a menu and asks the user to choose and option by typing a number" do
       terminal = double :terminal
@@ -22,24 +39,32 @@ describe Menu do
         end
         expect(terminal).to receive(:puts).with("Here's a list of shop items").ordered
         expect(repo_array.first).to eq "1 - Lego - 9.99 - 20"
-        expect(repo_array.last).to eq("3 - Magformers - 24.99 - 40")
+        expect(repo_array.last).to eq("2 - My Little Pony - 13.99 - 50")
         menu_choice = Menu.new(terminal)
         menu_choice.menu_1
       end
     end
-    xit "creates a new item if the menu choice is 2" do
-      io = double :io
+    it "creates a new item if the menu choice is 2" do
+      terminal = double :terminal      
       repo = ItemRepository.new
-      repo_array = []
-      repo.all.each do |record|
-        repo_array << "#{record.id} - #{record.item_name} - #{record.unit_price} - #{record.quantity}"
-      end
-      expect(io).to receive(:puts).with("Welcome to the shop management program!\n\nWhat would you like to do? (type a number)\n1 = list all shop items\n2 = create a new item\n3 = list all orders\n4 = create a new order").ordered
-      expect(io).to receive(:gets).and_return("1").ordered
-      expect(repo_array.first).to eq "1 - Lego - 9.99 - 20"
-      expect(repo_array.last).to eq("3 - Magformers - 24.99 - 40")
-      menu_choice = Menu.new(io)
-      menu_choice.run_menu
+      
+      expect(terminal).to receive(:puts).with("Let's create a new item!\nEnter a name for this item").ordered
+      expect(terminal).to receive(:gets).and_return("Moana Boat").ordered
+      expect(terminal).to receive(:puts).with("Enter a price for this item").ordered
+      expect(terminal).to receive(:gets).and_return("9.99").ordered
+      expect(terminal).to receive(:puts).with("Enter a quantity for this item").ordered
+      expect(terminal).to receive(:gets).and_return("30").ordered
+
+      new_item = Item.new
+      new_item.item_name = "Moana Boat"
+      new_item.unit_price = "9.99"
+      new_item.quantity = "30"
+
+      repo.create(new_item)
+
+      expect(repo.all.length).to eq 3
+      menu_choice = Menu.new(terminal)
+      menu_choice.menu_2
     end
   end
 end
