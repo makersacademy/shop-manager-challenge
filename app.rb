a532777b03814a71eb236032b0232377cdbc592d
Andy <ajscottlewis@gmail.com>
2022-11-27 18:17:27 +0000

    Add tests for and implement 3 of 4 app conditions

--- app.rb
+++ app.rb
@@ -1,29 +1,77 @@
-require 'lib/database_connection'
+require_relative 'lib/database_connection'
+require_relative 'lib/item_repository'
+require_relative 'lib/order_repository'
+require_relative 'lib/item'
+require_relative 'lib/order'
 
-# We need to give the database name to the method `connect`.
-DatabaseConnection.connect('items_orders')
+class Application
 
+  # The Application class initializer takes four arguments:
+  #  * The database name to call `DatabaseConnection.connect`
+  #  * the Kernel object as `io` (so we can mock the IO in our tests)
+  #  * the ItemRepository object (or a double of it)
+  #  * the OrderRepository object (or a double of it)
+  def initialize(database_name, io, item_repository, order_repository)
+    DatabaseConnection.connect(database_name)
+    @io = io
+    @item_repository = item_repository
+    @order_repository = order_repository
+  end
 
+  def run
+    # "Runs" the terminal application so it can ask the user to enter some input
+    # and then decide to run the appropriate action or behaviour.
 
+    # Use `@io.puts` or `@io.gets` to write output and ask for user input.
 
+    @io.puts "Welcome to the shop management program!\n"
 
+    @io.puts "What do you want to do?"
+    @io.puts " 1 = list all shop items"
+    @io.puts " 2 = create a new item"
+    @io.puts " 3 = list all orders"
+    @io.puts " 4 = create a new order\n"
+    
+    command = @io.gets.chomp
 
+    if command == "1"
+      @io.puts "\nHere's a list of all shop items:\n"
+      items = @item_repository.all
+      items.each do |item|
+        @io.puts " ##{item.id} #{item.name} - unit price: £#{item.unit_price} - quantity remaining: #{item.quantity}"
+      end
+    elsif command == "2"
+      item = Item.new
+      @io.puts "Please enter the following item information:"
+      @io.puts "Item name: "
+      item.name = @io.gets.chomp
+      @io.puts "Unit price: "
+      item.unit_price = @io.gets.chomp
+      @io.puts "Quantity remaining: "
+      item.quantity = @io.gets.chomp
+      # Add item to repo
+      @item_repository.create(item)
+      @io.puts "This item has been successfully added"
+    elsif command == "3"      
+      @io.puts "\nHere's a list of all orders:\n"
+      orders = @order_repository.all
+      orders.each do |order|
+        @io.puts " ##{order.id} Customer: #{order.customer_name} - item id: #{order.item_id} - date order was placed: #{order.date}"
+      end
+    elsif command == "4"
 
+    else
+      @io.puts "Only 1, 2, 3 and 4 are valid commands"
+    end
+  end
+end
 
-# Here's an example of the terminal output your program should generate (yours might be slightly different — that's totally OK):
-
-# Welcome to the shop management program!
-
-# What do you want to do?
-#   1 = list all shop items
-#   2 = create a new item
-#   3 = list all orders
-#   4 = create a new order
-
-# 1 [enter]
-
-# Here's a list of all shop items:
-
-#  #1 Super Shark Vacuum Cleaner - Unit price: 99 - Quantity: 30
-#  #2 Makerspresso Coffee Machine - Unit price: 69 - Quantity: 15
-#  (...)
+if __FILE__ == $0
+  app = Application.new(
+    'items_orders',
+    Kernel,
+    ItemRepository.new,
+    OrderRepository.new
+  )
+  app.run
+end
