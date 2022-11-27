dfbae43c288ac958a40669202dcae73f9efca4d5
Nicola Hearn <nicolahearn@MASTUDENT-Nicola-Hearn-C02G5H3VQ6L4.local>
2022-11-27 07:28:26 +0000

    updates app to point at database shop

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@
 require_relative 'lib/database_connection'
 
 # We need to give the database name to the method `connect`.
-DatabaseConnection.connect('music_library')
+DatabaseConnection.connect('shop')
 
 # Perform a SQL query on the database and get the result set.
-sql = 'SELECT id, title FROM albums;'
+sql = 'SELECT id, item_name, unit_price, quantity FROM items;'
 result = DatabaseConnection.exec_params(sql, [])
 
 # Print out each record from the result set .
