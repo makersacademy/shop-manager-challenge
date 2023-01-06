2e25361136444296864f89882d66253e970ebf82
Joe Burgess <josephburgess@gmail.com>
2023-01-06 16:03:40 +0000

    test driving item repository

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -2,8 +2,26 @@ require_relative 'item'
 
 class ItemRepository
   def all
+    sql = 'SELECT * FROM items ORDER BY id;'
+    results = DatabaseConnection.exec_params(sql, [])
+    items = []
+    results.each do |record|
+      items << record_to_object(record)
+    end
+    items
   end
   
   def create(item)
+  end
+
+  private
+
+  def record_to_object(record)
+    object = Item.new
+    object.id = record['id']
+    object.name = record['name']
+    object.price = record['price']
+    object.quantity = record['quantity']
+    object
   end
 end
