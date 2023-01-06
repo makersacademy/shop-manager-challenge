562993647b2e692313d8418b5ab3a44cbaf4c90b
Joe Burgess <josephburgess@gmail.com>
2023-01-06 16:07:54 +0000

    writing test for create method

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -23,4 +23,21 @@ RSpec.describe ItemRepository do
     expect(items[2].quantity).to eq '50'
   end
 
+  it 'creates a new item and adds to the item repository' do
+    repo = ItemRepository.new
+    items = repo.all
+
+    expect(items.length).to eq 6
+    expect(items.last.name).to eq 'Chicken Breast'
+
+    new_item = Item.new
+    new_item.name = 'Champagne'
+    new_item.price = '10'
+    new_item.quantity = '25'
+    repo.create(new_item)
+    updated_items = repo.all
+
+    expect(updated_items.length).to eq 7
+    expect(updated_items.last.name).to eq 'Champagne'
+  end
 end
