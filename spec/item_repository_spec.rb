7d40861cdfe8ed23f0d745da606b240f66399408
Joe Burgess <josephburgess@gmail.com>
2023-01-06 16:03:32 +0000

    writing first test for item repository

---
+++ spec/item_repository_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../lib/item_repository'
+
+RSpec.describe ItemRepository do
+  def reset_tables
+    seed_sql = File.read('spec/seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_manager_test' })
+    connection.exec(seed_sql)
+  end
+
+  before(:each) do
+    reset_tables
+  end
+
+  it 'provides a list of all items' do
+
+    repo = ItemRepository.new
+    
+    items = repo.all
+    expect(items.length).to eq 6
+    expect(items[0].name).to eq 'Apple'
+    expect(items[1].price).to eq '$1.00'
+    expect(items[3].name).to eq 'Crisp Pack'
+    expect(items[2].quantity).to eq '50'
+  end
+
+end
