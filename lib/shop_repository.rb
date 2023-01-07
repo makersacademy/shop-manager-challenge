2542f5d65d5c08d1df5f4de89d4ec050278b9914
Tom Rolfe <tomrolfe20@gmail.com>
2023-01-07 18:08:06 +0000

    add item test passed

--- lib/shop_repository.rb
+++ lib/shop_repository.rb
@@ -3,18 +3,23 @@ class ShopRepository
   def all_items
     sql = 'SELECT * FROM items'
     repo = DatabaseConnection.exec_params(sql, [])
-    items =
-      repo.map do |each_item|
-        item = Item.new
-        item.id = each_item['id']
-        item.name = each_item['name']
-        item.unit_price = each_item['unit_price']
-        item.quantity = each_item['quantity']
-      end
+    items = []
+    repo.each do |each_item|
+      item = Item.new
+      item.id = each_item['id']
+      item.name = each_item['name']
+      item.unit_price = each_item['unit_price']
+      item.quantity = each_item['quantity']
+      items << item
+    end
     items
   end
 
-  def add_item
+  def add_item(name, unit_price, quantity)
+    sql =
+      "INSERT INTO items (name, unit_price, quantity)
+    VALUES ('#{name}','#{unit_price}','#{quantity}');"
+    repo = DatabaseConnection.exec_params(sql, [])
   end
 
   def all_orders
--- spec/shop_repository_spec.rb
+++ spec/shop_repository_spec.rb
@@ -2,11 +2,27 @@ require 'shop_repository'
 require 'items'
 require 'orders'
 
+def reset_table
+  seed_sql = File.read('spec/seeds_cohort_test.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_test' })
+  connection.exec(seed_sql)
+end
+
 RSpec.describe ShopRepository do
-  before(:all) { @repo = ShopRepository.new }
+  before(:each) do
+    @repo = ShopRepository.new
+    reset_table
+  end
 
   it 'returns all shop items' do
     expect(@repo.all_items.length).to eq 3
+    expect(@repo.all_items[0].name).to eq 'Pickles'
+  end
+
+  it 'adds a new user' do
+    @repo.add_item('Popcorn', '5', '5')
+    expect(@repo.all_items.length).to eq 4
+    expect(@repo.all_items[-1].name).to eq 'Popcorn'
   end
 end
 
