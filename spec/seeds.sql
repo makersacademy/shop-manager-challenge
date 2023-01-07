e7a09e81c3c4e0150bfb8883804840decb31cf06
Joe Burgess <josephburgess@gmail.com>
2023-01-07 11:45:50 +0000

    adding items_orders data

--- spec/seeds.sql
+++ spec/seeds.sql
@@ -11,3 +11,19 @@ INSERT INTO items (name, price, quantity) VALUES ('Chocolate Bar', '3', '50');
 INSERT INTO items (name, price, quantity) VALUES ('Crisp Pack', '2', '200');
 INSERT INTO items (name, price, quantity) VALUES ('Sirloin Steak', '5', '10');
 INSERT INTO items (name, price, quantity) VALUES ('Chicken Breast', '4', '20');
+
+INSERT INTO items_orders (item_id, order_id) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 1),
+(2, 2),
+(3, 4),
+(3, 5),
+(4, 3),
+(4, 4),
+(4, 5),
+(5, 2),
+(5, 3),
+(5, 4),
+(6, 5);
