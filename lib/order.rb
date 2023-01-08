c8a0f502e1e26104f594fae01293313d3bbca27f
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-08 15:21:19 +0000

    Implemeted initialize and equality methods

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,12 @@
 class Order
   attr_accessor :id, :customer_name, :date, :item_id
+
+  def initialize(attributes = [nil] * 4)
+    @id, @customer_name, @date, @item_id = attributes
+    @attributes = [@id, @customer_name, @date, @item_id]
+  end
+
+  def ==(other)
+    @attributes = other.attributes
+  end
 end
