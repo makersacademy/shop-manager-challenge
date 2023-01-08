72e9a36632221c43b065e0deafe0744aafcf2b7f
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-08 18:46:06 +0000

    Fixed test for listing items

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -19,22 +19,22 @@ describe Application do
     @io,
     OrderRepository.new,
     ItemRepository.new)
-    expect(@io).to receive @welcome_str
-    expect(@io).to receive @selection_str
+    expect(@io).to receive(:puts).with(@welcome_str).ordered
+    expect(@io).to receive(:puts).with(@selection_str).ordered
   end
 
   after(:each) do
+    expect(@io).to receive(:puts).with(@selection_str).ordered
+    expect(@io).to receive(:gets).and_return("5").ordered
     @app.run
   end
   
   it "Application can list all items after formatting" do
+    expect(@io).to receive(:gets).and_return("1").ordered
     expect(@io).to receive(:puts).with("Here is the list of all shop items\n").ordered
     expect(@io).to receive(:puts).with("#1 Super Shark Vacuum Cleaner - Unit Price: £99.99").ordered
-    expect(@io).to receive(:puts).with("#2 Makerspresso Coffee Machine Unit Price: £69.50").ordered
-    expect(@io).to receive(:puts).with("#1 ThomasTech Wireless Charger - Unit Price: £11.39").ordered
-    expect(@io).to receive(:puts).with(@welcome_str).ordered
-    expect(@io).to receive(:gets).with("5").ordered
+    expect(@io).to receive(:puts).with("#2 Makerspresso Coffee Machine - Unit Price: £69.50").ordered
+    expect(@io).to receive(:puts).with("#3 ThomasTech Wireless Charger - Unit Price: £11.39").ordered
   end
-    
 
 end
