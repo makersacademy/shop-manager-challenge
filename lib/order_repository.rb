e4d5263929392459b4dea4fbd5f253cf94c50080
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-08 15:29:35 +0000

    Reworked ItemRepository class to implement OrderRepository

--- lib/order_repository.rb
+++ lib/order_repository.rb
@@ -1,11 +1,23 @@
+require_relative "database_connection"
+require_relative "order"
+
 class OrderRepository
 
   def all
-
+    query = "SELECT * FROM orders"
+    results = DatabaseConnection.exec_params(query, [])
+    results.map do |result|
+      Order.new([result["id"],
+      result["customer_name"],
+      result["date"],
+      result["item_id"]])
+    end
   end
 
-  def create
-
+  def create(order)
+    query = "INSERT INTO orders (customer_name, date, item_id) 
+    VALUES ('#{order.customer_name}', '#{order.date}', #{order.item_id})"
+    DatabaseConnection.exec_params(query, [])
   end
 
 end
