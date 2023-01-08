6aaeee9dffe9faf587ef9119aa6be3f6fe47a08a
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-08 14:23:52 +0000

    Created initialize method for easy assignment of instance variables

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,9 @@
 class Item
   attr_accessor :id, :name, :unit_price, :quantity
+
+  def initialize(attributes = [nil] * 4)
+    @id, @name, @unit_price, @quantity = attributes
+    @attributes = [@id, @name, @unit_price, @quantity]
+  end
+
 end
