d6a1a8b7655890595c4163c780a4e8570a59b3bc
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-08 17:58:32 +0000

    Implemented test for listing items

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,18 +3,19 @@ require_relative '../app'
 describe Application do
 
   before(:all) do
-    @welcome_str = "Welcome to the shop management program!\n\n"
-    @selection_str = "What do you want to do?\n" \
+    @welcome_str = "Welcome to the shop management program!\n"
+    @selection_str = "\nWhat do you want to do?\n" \
     "  1 = list all shop items\n" \
     "  2 = create a new item\n" \
     "  3 = list all orders\n" \
-    "  4 = create a new order"
+    "  4 = create a new order" \
+    "  5 = end the program"
   end
 
   before(:each) do
     @io = double :io
     @app = Application.new(
-    'shop_manager',
+    'shop_manager_test',
     @io,
     OrderRepository.new,
     ItemRepository.new)
@@ -27,6 +28,13 @@ describe Application do
   end
   
   it "Application is told to list all items" do
+    expect(@io).to receive(:puts).with("Here is the list of all shop items\n").ordered
+    expect(@io).to receive(:puts).with("#1 Super Shark Vacuum Cleaner - Unit Price: £99.99").ordered
+    expect(@io).to receive(:puts).with("#2 Makerspresso Coffee Machine Unit Price: £69.50").ordered
+    expect(@io).to receive(:puts).with("#1 ThomasTech Wireless Charger - Unit Price: £11.39").ordered
+    expect(@io).to receive(:puts).with(@welcome_str).ordered
+    expect(@io).to receive(:gets).with("5").ordered
+
   end
     
 
