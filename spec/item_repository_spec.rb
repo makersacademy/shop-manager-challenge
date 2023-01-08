d2b2cce85f6cc5fe4617842a4d51dfd923d1f22c
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-08 15:15:29 +0000

    Created test for #create

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -7,7 +7,7 @@ describe ItemRepository do
     @item1 = Item.new([1, "Super Shark Vacuum Cleaner",
     99.99, 3])
     @item2 = Item.new([2, "Makerspresso Coffee Machine",
-    69.50, 5])
+    69.5, 5])
     @item3 = Item.new([3, 'ThomasTech Wireless Charger',
     11.39, 1])
   end
@@ -21,6 +21,12 @@ describe ItemRepository do
 
   it "#all returns array of item objects for all items in database" do
     expect(@repo.all).to eq [@item1, @item2, @item3]
+  end
+
+  it "#create takes item instance and inserts into database" do
+    new_item = Item.new([4, "item", 1.0, 20])
+    @repo.create(new_item)
+    expect(@repo.all).to eq [@item1, @item2, @item3, new_item]
   end
 
 end
