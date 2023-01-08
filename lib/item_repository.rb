f952c0c6bff256f037739c219be68a733662b4bf
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-08 14:49:11 +0000

    Implemented #all

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,7 +1,17 @@
+require_relative "database_connection"
+require_relative "item"
+
 class ItemRepository
 
   def all
-
+    query = "SELECT * FROM items"
+    results = DatabaseConnection.exec_params(query, [])
+    results.map do |result|
+      Item.new([result["id"],
+      result["name"],
+      result["unit_price"],
+      result["quantity"]])
+    end
   end
 
   def create
