4a5bb7b9e6b5ae3a5d6722b03fa720cd5653355d
terrybro66 <terrybro61@gmail.com>
2023-01-10 08:44:41 +0000

    first commit

---
+++ lib/item_repository.rb
@@ -0,0 +1,8 @@
+require_relative "./item"
+class ItemRepository
+  def list_items
+  end
+
+  def create_items
+  end
+end
---
+++ lib/order.rb
@@ -0,0 +1,3 @@
+class Order
+  attr_accessor :id, :order_name, :date, :item_id
+end
