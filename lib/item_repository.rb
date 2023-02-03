a336ff5652c886a7369f0b8fc8e52149659b2061
Luke Lai <lukelai.dev@gmail.com>
2023-02-03 21:55:11 +0000

    Mofity to throw errors with item duplication and invalid inputs

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -21,6 +21,17 @@ class ItemRepository
   end
 
   def create(item)
+    # Check if the item name is already added in the database
+    check_sql = 'SELECT name FROM items WHERE name = $1;'
+    result_set = DatabaseConnection.exec_params(check_sql,[item.name])
+    result_set.each {|result| fail 'Item is already created' unless result.nil?}
+    #Check input
+    p item.quantity
+    if item.name == '' || item.price <= 0 || item.quantity <= 0
+      fail 'Invalid inputs'
+    end
+
+    # If no error, add to the database
     sql = 'INSERT INTO items (name,price,quantity) VALUES ($1,$2,$3);'
     params = [item.name, item.price, item.quantity]
 
--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -25,7 +25,7 @@ describe ItemRepository do
   end
 
   it 'creates a new item to the list of item objects' do
-    item = double :item, name:'Kiwi', price:'$5.00', quantity:20
+    item = double :item, name:'Kiwi', price:5, quantity:20
     repo = ItemRepository.new
 
     repo.create(item)
@@ -37,6 +37,24 @@ describe ItemRepository do
     expect(items.last.name).to eq 'Kiwi' 
     expect(items.last.price).to eq '$5.00'
     expect(items.last.quantity).to eq 20
+  end
 
+  it 'raises an error when trying to create an exisiting item' do
+    item = double :item, name:'Kiwi', price:5, quantity:20
+    repo = ItemRepository.new
+
+    repo.create(item)
+  expect {repo.create(item)}.to raise_error 'Item is already created'
+  end
+
+  it 'raises an error when trying to create item with invalid information' do
+    item_1 = double :item, name:'', price:5, quantity:20
+    item_2 = double :item, name:'Chocolate', price:0, quantity:20
+    item_3 = double :item, name:'Rice', price:10, quantity:0
+    repo = ItemRepository.new
+
+  expect {repo.create(item_1)}.to raise_error 'Invalid inputs'
+  expect {repo.create(item_2)}.to raise_error 'Invalid inputs'
+  expect {repo.create(item_3)}.to raise_error 'Invalid inputs'
   end
 end 
