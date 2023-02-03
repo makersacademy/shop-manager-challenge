021837ae0460e1429df1d7ca57f32ef772a1b609
addauk <addauk@gmail.com>
2023-02-03 16:37:31 +0000

    setup to generate and view coverage files

--- app.rb
+++ app.rb
@@ -115,12 +115,12 @@ end
 # run the following code if this is the main file being run, instead of having
 # been required or loaded by another file.
 # If you want to learn more about __FILE__ and $0, see here: https://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Variables_and_Constants#Pre-defined_Variables
-if __FILE__ == $0
-  app = Application.new(
-    'shop_manager',
-    Kernel,
-    ItemRepository.new,
-    OrderRepository.new
-  )
-  app.run
-end
+# if __FILE__ == $0
+#   app = Application.new(
+#     'shop_manager',
+#     Kernel,
+#     ItemRepository.new,
+#     OrderRepository.new
+#   )
+#   app.run
+# end
