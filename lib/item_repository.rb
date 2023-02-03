50b8414919e952ae4954327725e51ead39a052c2
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-02-03 16:38:09 +0000

    create model classes, create and test all method in item_repository.rb

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,1 +1,23 @@
 require_relative './item'
+
+class ItemRepository
+  def all
+    sql = 'SELECT * FROM items'
+    records = DatabaseConnection.exec_params(sql, [])
+    items = []
+
+    records.each do |record|
+      item = Item.new
+      item.id = record['id']
+      item.name = record['name']
+      item.price = record['price']
+      item.quantity = record['quantity']
+
+      items << item
+    end
+
+    return items
+  end
+
+
+end
--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -2,22 +2,22 @@ require 'item_repository'
 
 def reset_tables
   seed_sql = File.read('spec/seeds_items_orders.sql')
-  connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_manager' })
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_manager_test' })
   connection.exec(seed_sql)
 end
 describe ItemRepository do
   before(:each) do 
     reset_tables
   end
-  it "returns all items in table"
+  it "returns all items in table" do
     repo = ItemRepository.new
     items = repo.all
-    expect(items.length) to eq 4
-    expect(items[0].name).to eq 'Steak'
-    expect(items[0].quantity).to eq 10
+    expect(items.length).to eq 4
+    expect(items[0].name).to eq 'steak'
+    expect(items[0].quantity).to eq "10"
     expect(items[2].name).to eq 'chicken thigh'
-    expect(items[2].price).to eq 3
-    expect(items[3].id).to eq 4
+    expect(items[2].price).to eq "3"
+    expect(items[3].id).to eq "4"
   end
 end
 
