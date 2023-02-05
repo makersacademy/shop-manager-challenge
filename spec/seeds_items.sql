0cc4f532dce700f68291a3b8693c733078e36bf2
Hayley Dobbs <iamdobbs@icloud.com>
2023-02-05 21:36:51 +0000

    Updated seeds_items.sql to allow for fkrestraint when testing

--- spec/seeds_items.sql
+++ spec/seeds_items.sql
@@ -1,4 +1,6 @@
-TRUNCATE TABLE items RESTART IDENTITY; 
+TRUNCATE TABLE items, orders RESTART IDENTITY CASCADE; 
 
 INSERT INTO items (name, price, qty) VALUES ('Shirt', '15', '50');
 INSERT INTO items (name, price, qty) VALUES ('Sweat', '30', '100');
+INSERT INTO orders (customer, date, item_id) VALUES ('Dai Jones', '01/30/2023', '1');
+INSERT INTO orders (customer, date, item_id) VALUES ('Bobby Price', '02/03/2023', '2');
