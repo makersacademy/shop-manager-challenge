d939df4f393251dc01bd3ec557f7c798eef2d57e
mirandaweston <mweston105@yahoo.co.uk>
2023-02-06 00:20:04 +0000

    Adding OrderRepository having passed all methods

--- lib/order_repository.rb
+++ lib/order_repository.rb
@@ -37,4 +37,12 @@ class OrderRepository
   
     return order
   end
+
+  def create(order)
+    sql = 'INSERT INTO orders (customer_name, date, item_id) VALUES ($1, $2, $3);'
+    sql_params = [order.customer_name, order.date, order.item_id]
+  
+    result_set = DatabaseConnection.exec_params(sql, sql_params)
+    return nil
+  end
 end
--- spec/order_repository_spec.rb
+++ spec/order_repository_spec.rb
@@ -35,4 +35,23 @@ RSpec.describe OrderRepository do
     expect(order.date).to eq '2023-01-10 14:10:05'
     expect(order.item_id.to_i).to eq 1
   end
+
+  it "creates a new order" do
+    repo = OrderRepository.new
+  
+    new_order = Order.new
+    new_order.id = 4
+    new_order.customer_name = 'customer_4'
+    new_order.date = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+    new_order.item_id = 1
+  
+    repo.create(new_order)
+  
+    orders = repo.all
+  
+    expect(orders.last.id.to_i).to eq 4
+    expect(orders.last.customer_name).to eq 'customer_4'
+    expect(orders.last.date).to eq new_order.date
+    expect(orders.last.item_id.to_i).to eq 1
+  end
 end
