9ae032efc51d1442d2c54f69f0f9714946590a4c
jpates1 <jamesandrewpates@gmail.com>
2023-03-03 15:28:34 +0100

    Get all orders orders_repo_spec.rb test

--- spec/orders_repository_spec.rb
+++ spec/orders_repository_spec.rb
@@ -6,7 +6,27 @@ def reset_orders_table
   connection.exec(seed_sql)
 end
 
-describe PostsRepository do
+describe OrdersRepository do
   before(:each) do 
-    reset_posts_table
+    reset_orders_table
   end
+
+  # Get all Orders
+  it "Gets all orders" do
+    repo = OrdersRepository.new
+
+    orders = repo.all
+
+    orders.length # =>  2
+
+    expect(orders[0].id).to eq (1)
+    expect(orders[0].customer_name).to eq ('james pates')
+    expect(orders[0].order_date).to eq ('2023-03-03')
+    expect(orders[0].item_id).to eq ('1')
+
+    expect(orders[1].id).to eq (1)
+    expect(orders[1].customer_name).to eq ('james pates')
+    expect(orders[1].order_date).to eq ('2023-03-01')
+    expect(orders[1].item_id).to eq ('1')
+  end
+end
