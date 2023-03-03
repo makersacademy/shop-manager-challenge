ed726ea66af88e9768e85a78d50d8ae739174df1
Ayoub El-hamdoon <aze@MASTUDENT-Ayoub-Elhamdoon-W7JGKXKG9H.local>
2023-03-03 14:05:59 +0000

    write tests for item repository

---
+++ spec/seed_item.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE items, items_orders RESTART IDENTITY;
+
+INSERT INTO items (name, price, quantity) VALUES ('item1', 66.50, 70);
+
+INSERT INTO items (name, price, quantity) VALUES ('item2', 33.25, 35);
