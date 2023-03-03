bebd78e9f8f129d649709d0b61a0567e4af58f7a
jpates1 <jamesandrewpates@gmail.com>
2023-03-03 15:25:27 +0100

    Set up items_repo_spec.rb

---
+++ spec/items_repository_spec.rb
@@ -0,0 +1,12 @@
+require "items_repository"
+
+def reset_items_table
+  seed_sql = File.read('spec/seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_manager_test' })
+  connection.exec(seed_sql)
+end
+
+describe ItemsRepository do
+  before(:each) do 
+    reset_items_table
+  end
