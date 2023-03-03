64fa98bc4dc8d4e61d1ff7783e41de2d137a9d1b
jpates1 <jamesandrewpates@gmail.com>
2023-03-03 17:58:28 +0100

    Lists shop items  - application_spec.rb test WORKING

---
+++ lib/app.rb
@@ -0,0 +1,44 @@
+require_relative 'orders'
+require_relative 'orders_repository'
+require_relative 'items'
+require_relative 'items_repository'
+require_relative 'database_connection'
+
+
+class Application
+  def initialize(database_name, io, item_repository, order_repository)
+    DatabaseConnection.connect(database_name)
+    @io = io
+    @item_repository = item_repository
+    @order_repository = order_repository
+  end
+
+  def run
+    display_menu
+    choose_option
+  end
+
+  def display_menu
+    @io.puts "Welcome to the shop management program"
+    @io.puts "What do you want to do?"
+    @io.puts "1 = list all shop items"
+    @io.puts "2 = create a new item"
+    @io.puts "3 = list all shop orders"
+    @io.puts "4 = create a new  order"
+  end
+
+  def choose_option
+    option = @io.gets.chomp
+
+    case option
+      when "1"
+          list_all_shop_items
+    end
+  end
+
+  def list_all_shop_items
+    @item_repository.all.each do |item|
+     @io.puts "#{item.id} - #{item.item_name} - #{item.price} - #{item.quantity}"
+    end
+  end
+end
--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -1,4 +1,5 @@
-require './app.rb'
+require './lib/app.rb'
+
 def initialize
   @database = 'shop_manager_test'
 end
@@ -10,27 +11,23 @@ def reset_seeds_table
 end
 
 RSpec.describe Application do
+  let(:order_repo) { double("OrdersRepository")}
+  let(:item_repo) { double("ItemsRepository")}
+  let(:io) { double('io')}
+  let(:app) { Application.new(@database, io, item_repo, order_repo) }
   before(:each) do 
     reset_seeds_table
-    let(:order_repo) { double("OrdersRepository")}
-    let(:item_repo) { double("ItemsRepository")}
-    let(:io) { double('io')}
-    let(:app) { Application.new(@database, io, order_repo, item_repo) }
-    expect(io).to receive(:puts).with("Welcome to the shop management program")
-    expect(io).to receive(:puts).with("What do you want to do?")
-    expect(io).to receive(:puts).with("1 = list all shop items")
-    expect(io).to receive(:puts).with("2 = create a new item")
-    expect(io).to receive(:puts).with("3 = list all shop orders")
-    expect(io).to receive(:puts).with("4 = create a new  order")
   end
  
   it "gets a list of shop items when user selelects option 1" do
-    item1 = instance_double("Item", id: 1, item_name: "Apples", price: "2.05",
-      quantity: "5")
-    item2 = instance_double("Item", id: 2, item_name: "Pears", price: "5.02",
-      quantity: "3")
+    item1 = instance_double("Item", id: 1, item_name: "Apples", price: "2.05", quantity: "5")
+    item2 = instance_double("Item", id: 2, item_name: "Pears", price: "5.02", quantity: "3")
     allow(item_repo).to receive(:all).and_return([item1, item2])
-    expect(app.list_all_items[0]).to eq ("1 Apples - Unit Price: 2.05 - Quantity: 5")
+    allow(io).to receive(:puts)
+    expect(io).to receive(:gets).and_return("1\n") # stub gets method to return "1\n"
+    expect(io).to receive(:puts).with("1 - Apples - 2.05 - 5")
+    expect(io).to receive(:puts).with("2 - Pears - 5.02 - 3")
+    app.choose_option
   end
 
 end
