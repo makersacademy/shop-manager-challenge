57847682220b8b4c1ac5da8293971c197f5d7dcf
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-05 11:55:01 +0000

    created order repo that has all and find method

---
+++ lib/order_repo.rb
@@ -0,0 +1,32 @@
+
+class OrderRepository
+
+    # Selecting all records
+    # No arguments
+    def all
+      # Executes the SQL query:
+      #
+  
+      # Returns an array of Order objects.
+    end
+  
+    # Gets a single record by its ID
+    # One argument: the id (number)
+    def find(id)
+      # Executes the SQL query:
+      
+  
+      # Returns a single Order object.
+    end
+  
+    # Add more methods below for each operation you'd like to implement.
+  
+    # def create(order)
+    # end
+  
+    # def update(order)
+    # end
+  
+    # def delete(order)
+    # end
+  end
