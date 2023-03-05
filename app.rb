bc7bbc8da523ea8c55ad94fb8b6b607cd8299c36
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-05 14:21:54 +0000

    adding require for app

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 # file: app.rb
 require_relative 'lib/database_connection'
+require_relative 'lib/item_repository'
+require_relative 'lib/order_repository'
 
 # We need to give the database name to the method `connect`.
 DatabaseConnection.connect('orders')
