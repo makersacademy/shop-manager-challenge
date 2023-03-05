1950f10d3c0bcb479d74c012cb10b4db9133a70c
Valeria Serci <balerty84@gmail.com>
2023-03-05 17:16:25 +0000

    add test for .all in OrderRepository spec

--- spec/order_repository_spec.rb
+++ spec/order_repository_spec.rb
@@ -9,7 +9,26 @@ RSpec.describe OrderRepository do
   
   describe OrderRepository do
     before(:each) do 
-      reset_items_table
+      reset_orders_table
     end
+
+    it 'gets all orders' do
+      repo = OrderRepository.new
+      order = repo.all
+      expect(order.length).to eq(2)
+      expect(order[0].id).to eq(1)
+      expect(order[0].date).to eq('2023-02-06')
+      expect(order[0].customer_name).to eq('Paolo')
+      expect(order[0].item_id).to eq(1)
+
+      expect(order[1].id).to eq(2)
+      expect(order[1].date).to eq('2023-02-21')
+      expect(order[1].customer_name).to eq('Anna')
+      expect(order[1].item_id).to eq(2)
+    end
+
+
+
+
   end
 end
