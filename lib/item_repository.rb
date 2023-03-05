26dc1ddab162b59a625d1c9ccfaa7710b35d6dc1
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-05 12:51:17 +0000

    add code that initialises item data  puts in list

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -4,8 +4,18 @@ class ItemRepository
     # No arguments
     def all
       # Executes the SQL query:
-      
-  
+      result_set = DatabaseConnection.exec_params('SELECT id, item_name, unit_price, quantity, order_id')
+      items = []
+      result_set.each do |a_item|
+        item = Item.new
+        item.id = a_item['id']
+        item.item_name = a_item['item_name']
+        item.unit_price = a_item['unit_price']
+        item.quantity = a_item['quantity']
+        item.order_id = a_item['order_id']
+        items << item
+      end
+      return items
       # Returns an array of Item objects.
     end
   
