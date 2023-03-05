27c3675c77e1dbb7807d6aa325490bd128295ca8
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-05 10:36:51 +0000

    truncate table so table is empty between test runs

---
+++ spec/seeds.sql
@@ -0,0 +1,6 @@
+TRUNCATE TABLE orders RESTART IDENTITY; -- replace with your own table name.
+--this is so our table is emptied between each test run,
+-- so we can start with a fresh state.
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
