a2fdd8fda6c782d0f09c70f50fe04548dedc4474
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-05 11:56:08 +0000

    created item repo with all and find method

---
+++ lib/item_repo.rb
@@ -0,0 +1,31 @@
+class ItemRepository
+
+    # Selecting all records
+    # No arguments
+    def all
+      # Executes the SQL query:
+      
+  
+      # Returns an array of Item objects.
+    end
+  
+    # Gets a single record by its ID
+    # One argument: the id (number)
+    def find(id)
+      # Executes the SQL query:
+     
+  
+      # Returns a single Item object.
+    end
+  
+    # Add more methods below for each operation you'd like to implement.
+  
+    # def create(item)
+    # end
+  
+    # def update(item)
+    # end
+  
+    # def delete(item)
+    # end
+  end
