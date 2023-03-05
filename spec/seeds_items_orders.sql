13c481952322071d07b4ffd072c80a79f2addc32
Dilan Patel <patel_dilan@hotmail.com>
2023-03-05 18:53:24 +0000

    SQL seed data added

---
+++ spec/seeds_items_orders.sql
@@ -0,0 +1,18 @@
+-- Write your SQL seed here. 
+
+-- First, you'd need to truncate the table - this is so our table is emptied between each test run,
+-- so we can start with a fresh state.
+-- (RESTART IDENTITY resets the primary key)
+
+TRUNCATE TABLE items, orders RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO items (name, price, quantity) VALUES ('Super Shark Vacuum Cleaner', 99, 30);
+INSERT INTO items (name, price, quantity) VALUES ('Makerspresso Coffee Machine', 69, 15);
+INSERT INTO items (name, price, quantity) VALUES ('Toastie Maker', 30, 60);
+
+INSERT INTO orders (customer_name, order_date, item_id) VALUES ('Customer1', '01-Jan-2023', 1);
+INSERT INTO orders (customer_name, order_date, item_id) VALUES ('Customer2', '10-Jan-2023', 2);
+INSERT INTO orders (customer_name, order_date, item_id) VALUES ('Customer3', '20-Jan-2023', 3);
