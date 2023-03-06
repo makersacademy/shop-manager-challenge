7e40a84a0e6cbed69dc5b44291d69a0960ab2f8f
lizaebadi <liza-1@hotmail.co.uk>
2023-03-06 00:35:53 +0000

    initial seed data inserted in tables

--- spec/seeds_items_orders.sql
+++ spec/seeds_items_orders.sql
@@ -1,4 +1,4 @@
-TRUNCATE TABLE items, orders, items_orders RESTART IDENTITY; -- replace with your own table name.
+TRUNCATE TABLE items, orders RESTART IDENTITY; -- replace with your own table name.
 
 -- Below this line there should only be `INSERT` statements.
 -- Replace these statements with your own seed data.
@@ -8,15 +8,7 @@ INSERT INTO items (name, unit_price, item_quantity) VALUES ('Foundation', 42, 40
 INSERT INTO items (name, unit_price, item_quantity) VALUES ('Lipstick', 19, 15);
 INSERT INTO items (name, unit_price, item_quantity) VALUES ('Blusher', 22, 10);
 
-INSERT INTO orders (customer_name, order_date) VALUES ('Cindy', '2023-03-05');
-INSERT INTO orders (customer_name, order_date) VALUES ('Lucy', '2023-03-03');
-INSERT INTO orders (customer_name, order_date) VALUES ('Jane', '2023-03-01');
-
-INSERT INTO items_orders (item_id, order_id) VALUES (1, 3);
-INSERT INTO items_orders (item_id, order_id) VALUES (1, 2);
-INSERT INTO items_orders (item_id, order_id) VALUES (2, 2);
-INSERT INTO items_orders (item_id, order_id) VALUES (3, 1);
-INSERT INTO items_orders (item_id, order_id) VALUES (3, 3);
-INSERT INTO items_orders (item_id, order_id) VALUES (2, 1);
-INSERT INTO items_orders (item_id, order_id) VALUES (4, 1);
-INSERT INTO items_orders (item_id, order_id) VALUES (4, 3);
+INSERT INTO orders (customer_name, order_date, item_id) VALUES ('Cindy', '2023-03-05', 2);
+INSERT INTO orders (customer_name, order_date, item_id) VALUES ('Lucy', '2023-03-03', 2);
+INSERT INTO orders (customer_name, order_date, item_id) VALUES ('Jane', '2023-03-01', 1);
+INSERT INTO orders (customer_name, order_date, item_id) VALUES ('Alex', '2023-03-01', 4);
