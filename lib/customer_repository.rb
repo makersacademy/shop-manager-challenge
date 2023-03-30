a5a6c1f14feccf87ca3aa39ee122caf12c5ba601
umut <umut_baykan@hotmail.com>
2023-03-30 16:57:20 +0100

    non-mapped customer repository

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -21,12 +21,23 @@ class CustomerRepository
   end
 
   def all_orders
-    sql_statement = "SELECT orders.id, order_time, items.name, customers.name FROM orders
+    sql_statement = "SELECT order_time, items.name AS item_name, customers.name AS customer_name FROM orders
     JOIN items ON items.id = orders.item_id
     JOIN customers on customers.id = orders.customer_id
     ORDER BY order_time DESC"
     output = []
     results = DatabaseConnection.exec_params(sql_statement, [])
+    return results
+  end
+
+  def orders_of_customer(customer_name)
+    sql_statement = "SELECT orders.id, order_time, items.name FROM orders
+    JOIN items ON items.id = orders.item_id
+    JOIN customers on customers.id = orders.customer_id
+    WHERE customers.name = $1
+    ORDER BY order_time DESC"
+    output = []
+    results = DatabaseConnection.exec_params(sql_statement, [customer_name])
     return results
   end
 
@@ -48,7 +59,17 @@ class CustomerRepository
     orders = all_orders
     output = []
     orders.each { |order|
-      formatted_string = "Order ID: #{order["id"]}, Date: #{order["order_time"]}, Customer Name: #{order["name"]}"
+      formatted_string = "Date: #{order["order_time"]}, Order Item: #{order["item_name"]}, Customer Name: #{order["customer_name"]}"
+      output << formatted_string
+    }
+    return output
+  end
+
+  def list_of_items_ordered_by_customer(customer_name)
+    orders = orders_of_customer(customer_name)
+    output = []
+    orders.each { |order| 
+      formatted_string = "On #{order["order_time"]}, ordered #{order["name"]}"
       output << formatted_string
     }
     return output
--- spec/customer_repository_spec.rb
+++ spec/customer_repository_spec.rb
@@ -25,18 +25,27 @@ describe CustomerRepository do
   end
 
   context 'when reformatted' do
-    ### This section checks methods which format object into strings ###  
-    it 'returns the list of all customers and their names in a formatted string array' do
-      customers = repo.customer_list
-      expect(customers.length).to eq 5
-      expect(customers[0]).to eq "Customer ID: 1, Name: Customer_1"
-      expect(customers[-1]).to eq "Customer ID: 5, Name: Customer_5"
-    end   
+    # ### This section checks methods which format object into strings ###  
+    # it 'returns the list of all customers and their names in a formatted string array' do
+    #   customers = repo.customer_list
+    #   expect(customers.length).to eq 5
+    #   expect(customers[0]).to eq "Customer ID: 1, Name: Customer_1"
+    #   expect(customers[-1]).to eq "Customer ID: 5, Name: Customer_5"
+    # end   
 
-    it 'returns the list of all orders alongside their time and customer info in a formatted string array' do
-      orders = repo.order_list
-      expect(orders.length).to eq 18
-      expect(orders[0]).to eq "Order ID: 17, Date: 2023-03-01, Customer Name: Customer_4"
-    end
+    # it 'returns the list of all orders alongside their time, content and customer info in a formatted string array' do
+    #   ## Returns the array sorted by descending date order
+    #   orders = repo.order_list
+    #   expect(orders.length).to eq 18
+    #   expect(orders[0]).to eq "Date: 2023-03-01, Order Item: Peanut Butter, Customer Name: Customer_4"
+    #   expect(orders[-1]).to eq "Date: 2023-01-08, Order Item: Soap, Customer Name: Customer_3"
+    # end
+
+    # it 'returns a string list of all items ordered by a specific customer in a descending order based on order time' do
+    #   orders = repo.list_of_items_ordered_by_customer("Customer_3")
+    #   expect(orders.length).to eq 7
+    #   expect(orders[0]).to eq "On 2023-02-18, ordered Coal"
+    #   expect(orders[-2]).to eq "On 2023-01-08, ordered Chocolate"
+    # end
   end
 end
