9c12628284a4407ffa8f14c82bbc8afdeb34a548
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-03-31 17:27:19 +0100

    Implement #all method for OrderRepository

--- lib/order_repository.rb
+++ lib/order_repository.rb
@@ -1,6 +1,26 @@
+require_relative './database_connection'
+require_relative './order'
+
 class OrderRepository
+
+  def select_all
+    # Returns an array of hashes
+    sql = 'SELECT * FROM orders'
+    DatabaseConnection.exec_params(sql, [])
+  end
+
   def all
     # Returns an array of Order objects
+    orders = []
+
+    select_all.each do |row|
+      order = Order.new
+      order.customer_name = row['customer_name']
+      order.date = row['date']
+      orders << order
+    end
+
+    return orders
   end
 
   def create(item)
