5cd5ae10724c97b9182bebf5f3182e022917ffa7
natashabuckham <natashaisobel@gmail.com>
2023-04-01 14:51:02 +0100

    order repo spec set up

---
+++ spec/order_repository_spec.rb
@@ -0,0 +1,16 @@
+require 'order_repository'
+require 'item_repository'
+
+def reset_orders_table
+  seed_sql = File.read('spec/seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_manager_test' })
+  connection.exec(seed_sql)
+end
+
+RSpec.describe OrderRepository do
+  before(:each) do
+    reset_orders_table
+  end
+
+  
+end
