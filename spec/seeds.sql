e58187773a57f1b5921bd4461b9c3dd1bc856099
natashabuckham <natashaisobel@gmail.com>
2023-04-01 12:18:41 +0100

    seed data added to databases

---
+++ spec/seeds.sql
@@ -0,0 +1,24 @@
+TRUNCATE TABLE items, orders, items_orders RESTART IDENTITY;
+
+INSERT INTO items (name, price, quantity) VALUES
+('milk', 2, 50),
+('bread', 3, 30),
+('cake', 9, 10),
+('bananas', 4, 100),
+('broccoli', 1, 45);
+
+INSERT INTO orders (customer, date) VALUES
+('Quack Overflow', '04/01/23'),
+('Scrooge McDuck', '03/31/23'),
+('Silly Goose', '03/30/23');
+
+INSERT INTO items_orders (item_id, order_id) VALUES
+(1, 1),
+(2, 1),
+(2, 3),
+(3, 1),
+(4, 1),
+(4, 3),
+(5, 1),
+(5, 2),
+(5, 3);
