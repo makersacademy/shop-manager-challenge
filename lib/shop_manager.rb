bea9b760ba3eba032dd8f42b428f222051a94cc1
Manuela Iacobovici <miacobovici@yahoo.co.uk>
2023-04-02 19:33:03 +0100

    add and print items work

--- lib/shop_manager.rb
+++ lib/shop_manager.rb
@@ -1,17 +1,26 @@
+require_relative './item'
+require_relative './order'
+require 'pg'
+
 class ShopManager
-  WHAT_TO_DO = '    What do you want to do?
+  WHAT_TO_DO = 'What do you want to do?
     1 = list all shop items
     2 = create a new item
     3 = list all orders
     4 = create a new order'
 
   def initialize
-    @conn_db = psql -h 127.0.0.1 shop_manager
-    print '
-    Welcome to the shop management program!
+    db_connect
+    initiate_menu
+  end
 
-    ${WHAT_TO_DO}
-    '
+  def initiate_menu(initial = true)
+    puts "
+    Welcome to the shop management program!
+    " if initial == true
+    puts "
+    #{WHAT_TO_DO}
+    "
     answer = gets.chomp
     case answer
     when '1'
@@ -23,7 +32,7 @@ class ShopManager
     when '4'
       new_order_collect_data
     else
-      print(WHAT_TO_DO)
+      initiate_menu(false)
     end
   end
 
@@ -34,14 +43,14 @@ class ShopManager
       break if name.count("a-zA-Z") > 0
     end
     while true do
-      puts 'Enter the price for the ${name}'
+      puts "Enter the price for the #{name}"
       unit_price = gets.chomp
-      break if unit_price.is_a? Integer
+      break if unit_price.to_i.is_a? Integer
     end
     while true do
-      puts 'Enter the quantity for the ${name}'
+      puts "Enter the quantity for the #{name}"
       quantity = gets.chomp
-      break if quantity.is_a? Integer
+      break if quantity.to_i.is_a? Integer
     end
     item = Item.new(name, unit_price, quantity)
     new_item(item)
@@ -50,11 +59,13 @@ class ShopManager
   def new_item(item)
     # add item to items table
     query = "INSERT INTO items(name, unit_price, quantity) 
-    VALUES('${item.name}', '${item.unit_price}', '${item.quantity}');"
-    @conn_db.exec(query)
-    if @@ROWCOUNT > 0
-      puts "Your item has been added succesfully"
-    end
+    VALUES('#{item.name}', #{item.unit_price.to_i}, #{item.quantity.to_i});"
+    result = @conn_db.exec(query)
+    puts "Your item has been added succesfully"
+  end
+
+  def db_connect()
+    @conn_db = PG.connect({ host: '127.0.0.1', dbname: 'shop_manager' })
   end
 
   def new_order_collect_data
@@ -82,23 +93,23 @@ class ShopManager
   def new_order(order)
     # add order to orders table
     query = "INSERT INTO orders(customer_name, item_id) 
-    VALUES('${order.customer_name}', '${order.item_id}');"
-    @conn_db.exec(query)
-    if @@ROWCOUNT > 0
-      puts "Your order has been placed succesfully, thank you!"
-    end
+    VALUES('#{order.customer_name}', '#{order.item_id}') RETURNING *;"
+    p @conn_db.exec(query)
+    # if @@ROWCOUNT > 0
+    #   puts "Your order has been placed succesfully, thank you!"
+    # end
   end
 
   private
   def get_select_query(table)
-    "SELECT * FROM ${table};"
+    "SELECT * FROM #{table};"
   end
 
   def all_items
     query = get_select_query('items')
     items = @conn_db.exec(query)
     items = items.map do |item|
-      Item.new(item.name, item.unit_price, item.quantity, item.item_id)
+      Item.new(item['name'], item['unit_price'], item['quantity'], item['item_id'])
     end
     print_all_items(items)
     items
@@ -110,7 +121,7 @@ class ShopManager
     orders = @conn_db.exec(query)
     orders = orders.map do |order|
       item = items.find { |curr_item| curr_item.item_id == order.item_id }
-      Order.new(order.customer_name, item, order.order_id)
+      Order.new(order["customer_name"], item, order["order_id"])
     end
     print_all_orders(orders)
     orders
@@ -119,14 +130,17 @@ class ShopManager
   def print_all_items(items)
     #1 Super Shark Vacuum Cleaner - Unit price: 99 - Quantity: 30
     items.each do |item|
-      puts "${item.item_id} ${item.name} - Unit price: ${item.unit_price} - Quantity: ${item.quantity}"
+      puts "#{item.item_id} #{item.name} - Unit price: #{item.unit_price} - Quantity: #{item.quantity}"
     end
   end
 
   def print_all_orders(orders)
     #1 Super Shark Vacuum Cleaner - Unit price: 99 - Quantity: 30
     orders.each do |order|
-      puts "${order.order_id} ${order.customer_name} - Item: ${order.item.name} - Price: ${order.item.unit_price}"
+      puts order#"#{order.order_id} #{order.customer_name} - Item: #{order.item['name']} - Price: #{order.item.unit_price}"
     end
   end
 end
+
+ShopManager.new
+
