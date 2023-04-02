fcb675f1070a1308d37227f8305d75da06f1b75a
Quddus Rahman <qrahman115@gmail.com>
2023-04-02 18:25:31 +0100

    completed order repo tests

--- lib/order_repository.rb
+++ lib/order_repository.rb
@@ -4,35 +4,51 @@ class OrderRepository
   def all
     sql = 'SELECT * FROM orders'
     result_set = DatabaseConnection.exec_params(sql, [])
-
+  
     orders = []
-
+  
     result_set.each do |record|
-      order = Order.new
-      order.id = record['id'].to_i
-      order.customer_name = record['customer_name']
-      order.item_id = record['item_id'].to_i
-      order.date = record['date']
-
+      order = Order.new(
+        id: record['id'].to_i,
+        customer_name: record['customer_name'],
+        item_id: record['item_id'].to_i,
+        date: record['date']
+      )
+  
       orders << order
     end
-
+  
     return orders
   end
 
   def find(id)
-    # Implement method to find an order by id
+    sql = 'SELECT * FROM orders WHERE id = $1'
+    result_set = DatabaseConnection.exec_params(sql, [id])
+
+    return nil if result_set.ntuples.zero?
+
+    record = result_set.first
+    Order.new(
+      id: record['id'].to_i,
+      customer_name: record['customer_name'],
+      item_id: record['item_id'].to_i,
+      date: record['date']
+    )
   end
 
   def create(order)
-    # Implement method to create a new order
+    sql = 'INSERT INTO orders (customer_name, item_id, date) VALUES ($1, $2, $3) RETURNING id'
+    result = DatabaseConnection.exec_params(sql, [order.customer_name, order.item_id, order.date])
+    order.id = result[0]['id'].to_i
   end
 
   def update(id, order)
-    # Implement method to update an existing order
+    sql = 'UPDATE orders SET customer_name = $1, item_id = $2, date = $3 WHERE id = $4'
+    DatabaseConnection.exec_params(sql, [order.customer_name, order.item_id, order.date, id])
   end
 
   def delete(id)
-    # Implement method to delete an order by id
+    sql = 'DELETE FROM orders WHERE id = $1'
+    DatabaseConnection.exec_params(sql, [id])
   end
 end
--- spec/order_repository_spec.rb
+++ spec/order_repository_spec.rb
@@ -24,25 +24,53 @@ RSpec.describe OrderRepository do
 
   describe '#find' do
     it 'finds an order by id' do
-      # Implement test for 'find' method
+      order_repository = OrderRepository.new
+      order = order_repository.find(1)
+
+      expect(order).not_to be_nil
+      expect(order.id).to eq(1)
     end
   end
 
   describe '#create' do
     it 'creates a new order' do
-      # Implement test for 'create' method
+      order_repository = OrderRepository.new
+      new_order = Order.new(customer_name: 'John Doe', item_id: 1, date: '2023-05-01') # Update this line
+      order_repository.create(new_order)
+  
+      expect(new_order.id).not_to be_nil
+  
+      orders = order_repository.all
+      expect(orders.length).to eq(3)
     end
-  end
+  end  
 
   describe '#update' do
     it 'updates an existing order' do
-      # Implement test for 'update' method
+      order_repository = OrderRepository.new
+      order = order_repository.find(1)
+      order.customer_name = 'Jane Doe'
+      order.item_id = 2
+      order.date = '2023-06-01'
+
+      order_repository.update(order.id, order)
+      updated_order = order_repository.find(1)
+
+      expect(updated_order.customer_name).to eq('Jane Doe')
+      expect(updated_order.item_id).to eq(2)
+      expect(updated_order.date).to eq('2023-06-01')
     end
   end
 
   describe '#delete' do
     it 'deletes an order by id' do
-      # Implement test for 'delete' method
+      order_repository = OrderRepository.new
+      order_repository.delete(1)
+
+      expect(order_repository.find(1)).to be_nil
+
+      orders = order_repository.all
+      expect(orders.length).to eq(1)
     end
   end
 end
