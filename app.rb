153466f6db276f32b0d34788eaa166170f27802e
Gary Solloway <124901657+GarySolloway@users.noreply.github.com>
2023-04-02 14:14:07 +0100

    Refactored app.rb

--- app.rb
+++ app.rb
@@ -12,48 +12,56 @@ class Application
 
   def run
     @io.puts "Welcome to the shop management program!"
-    @io.puts ""
-    @io.puts "What do you want to do?"
-    @io.puts "1 - list all shop items"
-    @io.puts "2 - create a new item"
-    @io.puts "3 - list all orders"
-    @io.puts "4 - create a new order"
-    @io.puts "any other key to exit"
-    choice = @io.gets.chomp.to_i
 
-    if choice == 1
-      @io.puts "Here's a list of all shop items:"
-      @item_repository.all.each_with_index do |item, index| 
-        @io.puts "##{index + 1} #{item.name} - Unit price: £#{item.unit_price} - Quantity: #{item.quantity}"
+    while true
+      @io.puts ""
+      @io.puts "What do you want to do?"
+      @io.puts "1 - list all shop items"
+      @io.puts "2 - create a new item"
+      @io.puts "3 - list all orders"
+      @io.puts "4 - create a new order"
+      @io.puts "any other key to exit"
+      choice = @io.gets.chomp.to_i
+
+      case choice
+      when 1
+        @io.puts "Here's a list of all shop items:"
+        @item_repository.all.each_with_index do |item, index| 
+          @io.puts "##{index + 1} #{item.name} - Unit price: £#{item.unit_price} - Quantity: #{item.quantity}"
+        end
+
+      when 2
+        @io.puts "Enter the item name:"
+        name = @io.gets.chomp
+        @io.puts "Enter the item's unit price:"
+        unit_price = @io.gets.chomp.to_f.round(2)
+        @io.puts "Enter the item's quantity:"
+        quantity = @io.gets.chomp
+
+        item = Item.new
+        item.name = name
+        item.unit_price = unit_price
+        item.quantity = quantity
+        @item_repository.create(item)
+        @io.puts "'#{name}' was added to the database!"
+
+      when 3
+        @io.puts "Here's a list of all shop orders:"
+        @order_repository.all.each_with_index { |order, index| @io.puts "##{index + 1} Customer name: #{order.customer_name} - Order Date: #{order.order_date}" }
+
+      when 4
+        @io.puts "Enter the customer's name:"
+        name = @io.gets.chomp
+
+        order = Order.new
+        order.customer_name = name
+        order.order_date = Time.now.strftime("%Y-%m-%d")
+        @order_repository.create(order)
+        @io.puts "An order for '#{name}' was created!"
+        
+      else
+        abort("Goodbye!")
       end
-    elsif choice == 2
-      @io.puts "Enter the item name:"
-      name = @io.gets.chomp
-      @io.puts "Enter the item's unit price:"
-      unit_price = @io.gets.chomp.to_f.round(2)
-      @io.puts "Enter the item's quantity:"
-      quantity = @io.gets.chomp
-
-      item = Item.new
-      item.name = name
-      item.unit_price = unit_price
-      item.quantity = quantity
-      @item_repository.create(item)
-      @io.puts "'#{name}' was added to the database!"
-    elsif choice == 3
-      @io.puts "Here's a list of all shop orders:"
-      @order_repository.all.each_with_index { |order, index| @io.puts "##{index + 1} Customer name: #{order.customer_name} - Order Date: #{order.order_date}" }
-    elsif choice == 4
-      @io.puts "Enter the customer's name:"
-      name = @io.gets.chomp
-
-      order = Order.new
-      order.customer_name = name
-      order.order_date = Time.now.strftime("%Y-%m-%d")
-      @order_repository.create(order)
-      @io.puts "An order for '#{name}' was created!"
-    else
-      abort("Goodbye!")
     end
   end
 end
