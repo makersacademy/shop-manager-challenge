013058569139972395d47408daf8381010ea3e5b
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-04-03 02:51:05 +0100

    Formatted Code

--- app.rb
+++ app.rb
@@ -15,8 +15,7 @@ class Application
   def print_out_all_items
     items_for_sale = @shop_repository.all_items
     items_for_sale.each_with_index do |item, index| 
-      @io.puts 
-      "#{index + 1}: #{item.name} - Unit Price: £#{item.price.to_f / 100} - Quantity: #{item.qty}"
+      @io.puts "#{index + 1}: #{item.name} - Unit Price: £#{sprintf('%.2f', item.price.to_f / 100)} - Quantity: #{item.qty}"
     end
   end
 
@@ -42,23 +41,19 @@ class Application
     new_orders.each do |order_item| 
       order_list << [next_id, order_item]
     end
-    @io.puts "Enter your name:"
-    name = @io.gets.chomp!
-    @io.puts "Enter the date (in MMM-DD-YYYY):"
-    date = @io.gets.chomp!
+    @io.puts "Enter your name:"; name = @io.gets.chomp!
+    @io.puts "Enter the date (in MMM-DD-YYYY):"; date = @io.gets.chomp!
     new_order = Order.new; new_order.customer = name
     new_order.date_of_order = date; new_order.order_items = order_list
-    result = repo.create_order(new_order)
+    @order_repository.create_order(new_order)
   end
 
   def add_items_to_order
     output = []
     while true
-      @io.puts "Enter number of item to add: "
-      selection = @io.gets.chomp!
+      @io.puts "Enter number of item to add:"; selection = @io.gets.chomp!
       output << selection.to_i
-      @io.puts "add again? y/n"
-      again = gets.chomp!
+      @io.puts "add again? y/n"; again = gets.chomp!
       if again.include?("n")
         break
       end
@@ -67,40 +62,32 @@ class Application
   end
 
   def create_new_item
-    @io.puts "Enter item name:"
-    name = @io.gets.chomp!
-    @io.puts "Enter item price:"
-    price = @io.gets.chomp!
-    @io.puts "Enter item quantity:"
-    qty = @io.gets.chomp!
+    @io.puts "Enter item name:"; name = @io.gets.chomp!
+    @io.puts "Enter item price:"; price = @io.gets.chomp!
+    @io.puts "Enter item quantity:"; qty = @io.gets.chomp!
     new_item = ShopItem.new
-    new_item.name = name
-    new_item.price = price
-    new_item.qty = qty
+    new_item.name = name; new_item.price = price; new_item.qty = qty
     return new_item
+  end
+
+  def print_text_options
+    @io.puts "What do you want to do?"
+    @io.puts "1 = list all shop items"
+    @io.puts "2 = create a new item"
+    @io.puts "3 = list all orders"
+    @io.puts "4 = create a new order"
   end
 
   def run
     @io.puts "Welcome to the shop management program!"
-    @io.puts "What do you want to do?\n1 = list all shop items\n2 = create a new item\n3 = list all orders\n4 = create a new order"
-    selection = @io.gets.chomp!
+    print_text_options; selection = @io.gets.chomp!
     case selection
-      when "1"
-        print_out_all_items
-      when "2"
-        @shop_repository.create_item(create_new_item)
-      when "3"
-        print_out_all_orders
-      when "4"
-        puts "TBC"
-      else
-        puts "i don't understand..."
+      when "1"; print_out_all_items
+      when "2"; @shop_repository.create_item(create_new_item)
+      when "3"; print_out_all_orders
+      when "4"; create_new_order
+      else; puts "i don't understand..."
     end
-    # add_items_to_order  
-    # print_out_all_orders
-    # print_out_order(@order_repository.all_orders[0].id)
-    # print_out_all_items
-    # print @order_repository.all_orders[0].customer
   end
 end
 
