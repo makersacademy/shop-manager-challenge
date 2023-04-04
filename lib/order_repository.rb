fa8bd3f0bb72a87e39d871f265553b8e327aa9bd
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-04-04 07:45:11 +0100

    Refactor OrderRepository methods

--- lib/order_repository.rb
+++ lib/order_repository.rb
@@ -31,35 +31,8 @@ class OrderRepository
 
   def print_all_with_items
     # Returns an array of strings formatted to print with puts
-    sql = 'SELECT orders.id AS "order_id", customer_name, date, items.name, items.unit_price FROM orders
-                JOIN items_orders ON orders.id = order_id
-                JOIN items ON item_id = items.id;'
-    result_set = DatabaseConnection.exec_params(sql, [])
-    orders = []
-    order_ids = []
-    result_set.sort_by { |row| row["order_id"] }.each do |hash|
-      if !order_ids.include? hash["order_id"].to_i
-        order_ids << hash["order_id"].to_i
-        order = Order.new
-        order.id = hash["order_id"].to_i
-        order.customer_name = hash["customer_name"]
-        order.date = hash["date"]
-        orders << order
-      end
-    end
-
-    result_set.each do |hash|
-      orders.each do |order|
-        if order.id == hash["order_id"].to_i
-          item = Item.new
-          item.name = hash["name"]
-          item.unit_price = hash["unit_price"]#.to_f.round(2)
-          order.items << item
-        end
-      end
-    end
     output = []
-    orders.map do |order|
+    select_all_with_items.map do |order|
       output << " Order ##{order.id} - #{order.customer_name} - #{order.date}\n   Items:"
       order.items.each do |item|
         output << "     #{item.name}, £#{item.unit_price}"
@@ -69,6 +42,36 @@ class OrderRepository
 
   end
 
-  #private
+  private
 
+  def select_all_with_items
+    sql = 'SELECT orders.id AS "order_id", customer_name, date, items.name, items.unit_price FROM orders
+                  JOIN items_orders ON orders.id = order_id
+                  JOIN items ON item_id = items.id;'
+      result_set = DatabaseConnection.exec_params(sql, [])
+      orders = []
+      order_ids = []
+      result_set.sort_by { |row| row["order_id"] }.each do |hash|
+        if !order_ids.include? hash["order_id"].to_i
+          order_ids << hash["order_id"].to_i
+          order = Order.new
+          order.id = hash["order_id"].to_i
+          order.customer_name = hash["customer_name"]
+          order.date = hash["date"]
+          orders << order
+        end
+      end
+
+      result_set.each do |hash|
+        orders.each do |order|
+          if order.id == hash["order_id"].to_i
+            item = Item.new
+            item.name = hash["name"]
+            item.unit_price = hash["unit_price"]#.to_f.round(2)
+            order.items << item
+          end
+        end
+      end
+    return orders
+  end
 end
