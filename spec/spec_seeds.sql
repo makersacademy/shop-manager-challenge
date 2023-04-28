43734dadb916ee4f8d5e428ab86f48c7ef343225
Evan Thomas <evanthomas0292@gmail.com>
2023-04-28 13:39:11 +0100

    Edit the spec/spec_seeds insertion values

--- spec/spec_seeds.sql
+++ spec/spec_seeds.sql
@@ -3,18 +3,18 @@ TRUNCATE TABLE items, orders, items_orders RESTART IDENTITY;
 INSERT INTO items (name, unit_price, quantity)
     VALUES
         ('milk', 175, 20),
-        ('spinach', 115, 20),
-        ('pineapple', 95, 20),
-        ('chocolate', 210, 20),
-        ('cereal', 185, 20),
-        ('coffee', 450, 20);
+        ('spinach', 115, 1),
+        ('pineapple', 95, 0),
+        ('chocolate', 210, 16),
+        ('cereal', 185, 45),
+        ('coffee', 450, 2);
 
 INSERT INTO orders (customer_name, date_placed)
     VALUES
         ('Alice', '2021-02-05'),
-        ('Bob', '2021-02-05'),
-        ('Carol', '2021-02-05'),
-        ('Dom', '2021-02-05');
+        ('Bob', '2022-03-06'),
+        ('Carol', '2022-06-21'),
+        ('Dom', '2023-10-16');
 
 INSERT INTO items_orders (item_id, order_id)
     VALUES
