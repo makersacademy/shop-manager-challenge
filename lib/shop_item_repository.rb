72b681b1be733bfd37f3e5064ab0cd01f55d4d08
sarahc-dev <sarahclements@hotmail.co.uk>
2023-04-28 13:32:25 +0100

    rubocop refactor

--- lib/shop_item_repository.rb
+++ lib/shop_item_repository.rb
@@ -8,13 +8,19 @@ class ShopItemRepository
 
     shop_items = []
     result_set.each do |record|
-      shop_item = ShopItem.new
-      shop_item.id = record['id']
-      shop_item.name = record['name']
-      shop_item.unit_price = record['unit_price']
-      shop_item.quantity = record['quantity']
-      shop_items << shop_item
+      shop_items << create_shop_item(record)
     end
     return shop_items
+  end
+
+  private
+  
+  def create_shop_item(record)
+    shop_item = ShopItem.new
+    shop_item.id = record['id']
+    shop_item.name = record['name']
+    shop_item.unit_price = record['unit_price']
+    shop_item.quantity = record['quantity']
+    return shop_item
   end
 end
