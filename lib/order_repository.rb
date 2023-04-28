dd1c561eb619d5c6f94699c718ff9c6c2fab8207
Evan Thomas <evanthomas0292@gmail.com>
2023-04-28 16:10:09 +0100

    Write test for and implement OrderRepository#all

--- lib/order_repository.rb
+++ lib/order_repository.rb
@@ -1,2 +1,24 @@
+require_relative "./order"
+
 class OrderRepository
+  def all
+    sql = "SELECT * FROM orders;"
+    records = DatabaseConnection.exec_params(sql, [])
+    orders = []
+
+    records.each do |record|
+      order = Order.new
+      order.id = record["id"]
+      order.customer_name = record["customer_name"]
+      order.date_placed = record["date_placed"]
+      orders << order
+    end
+    orders
+  end
+
+  def all_with_items
+  end
+
+  def create
+  end
 end
--- spec/order_repository_spec.rb
+++ spec/order_repository_spec.rb
@@ -1,8 +1,32 @@
 require "order_repository"
 
+def reset_tables
+  sql_seeds = File.read("spec/spec_seeds.sql")
+  connection = PG.connect({ host: "127.0.0.1", dbname: "shop_manager_test" })
+  connection.exec(sql_seeds)
+end
+
 RSpec.describe OrderRepository do
+  before(:each) do
+    reset_tables
+  end
+
   describe "#all" do
-    xit "returns a list of all orders" do
+    it "returns a list of all orders" do
+      repo = OrderRepository.new
+      orders = repo.all
+
+      expect(orders.length).to eq 4
+
+      order_1 = orders.first
+      expect(order_1.id).to eq "1"
+      expect(order_1.customer_name).to eq "Alice"
+      expect(order_1.date_placed).to eq "2021-02-05"
+      
+      order_2 = orders.last
+      expect(order_2.id).to eq "4"
+      expect(order_2.customer_name).to eq "Dom"
+      expect(order_2.date_placed).to eq "2023-10-16"
     end
   end
 
