ac7d4ed908db8cb1881f3bf8b0187b4aaa341c4f
Evan Thomas <evanthomas0292@gmail.com>
2023-04-29 20:26:18 +0100

    Test drive Application#run to print out all items

--- lib/application.rb
+++ lib/application.rb
@@ -1,10 +1,33 @@
+require_relative "./item_repository"
+require_relative "./order_repository"
+require_relative "./database_connection"
+
 class Application
-  def initialize(io = Kernal)
+  def initialize(
+    database = "shop_manager",
+    io = Kernel,
+    item_repository = ItemRepository.new,
+    order_repository = OrderRepository.new
+  )
+    DatabaseConnection.connect(database)
     @io = io
+    @item_repository = item_repository
+    @order_repository = order_repository
   end
 
   def run
     print_welcome_options
+    choice = @io.gets.chomp
+    items = @item_repository.all
+    @io.puts("Here's a list of all shop items:\n")
+    items.each_with_index do |item, i|
+      price = item.unit_price.to_i
+      pounds = price / 100
+      pence = price % 100
+      formatted_price = "£#{pounds}.#{pence}"
+      @io.puts "##{i + 1} #{item.name} - Unit price: #{formatted_price} - Quantity: #{item.quantity}"
+    end
+
   end
   
   private
--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -2,26 +2,56 @@ require "application"
 
 RSpec.describe Application do
   describe "#run" do
-    it "welcomes the user and prints out the options" do
-      fake_io = double :fake_io
-      expect(fake_io).to receive(:puts).with("Welcome to the shop management program!\n")
-      expect(fake_io).to receive(:puts).with("What do you want to do?")
-      expect(fake_io).to receive(:puts).with("  1 = list all shop items")
-      expect(fake_io).to receive(:puts).with("  2 = create a new item")
-      expect(fake_io).to receive(:puts).with("  3 = list all orders")
-      expect(fake_io).to receive(:puts).with("  4 = create a new order")
-      expect(fake_io).to receive(:puts).with("")
+    context "when user chooses option 1" do
+      it "prints all the shop items when user inputs option" do
+        fake_io = double :fake_io
+        expect(fake_io).to receive(:puts).with("Welcome to the shop management program!\n")
+        expect(fake_io).to receive(:puts).with("What do you want to do?")
+        expect(fake_io).to receive(:puts).with("  1 = list all shop items")
+        expect(fake_io).to receive(:puts).with("  2 = create a new item")
+        expect(fake_io).to receive(:puts).with("  3 = list all orders")
+        expect(fake_io).to receive(:puts).with("  4 = create a new order")
+        expect(fake_io).to receive(:puts).with("")
+        fake_item_repo = double :fake_item_repo
+        fake_order_repo = double :fake_order_repo
 
-      app = Application.new(fake_io)
-      app.run
+        fake_item1 = double(
+          :fake_item1,
+          id: "3",
+          name: "name1",
+          unit_price: "340",
+          quantity: "3"
+        ) 
+        
+        fake_item2 = double(
+          :fake_item2,
+          id: "4",
+          name: "name2",
+          unit_price: "55",
+          quantity: "101"
+        )
+
+        items = [fake_item1, fake_item2]
+        allow(fake_item_repo).to receive(:all).and_return(items)
+        
+        allow(fake_io).to receive(:gets).and_return("1\n")
+        
+        expect(fake_io).to receive(:puts).with("Here's a list of all shop items:\n")
+        expect(fake_io).to receive(:puts).with(
+          "#1 name1 - Unit price: £3.40 - Quantity: 3"
+        )
+        expect(fake_io).to receive(:puts).with(
+          "#2 name2 - Unit price: £0.55 - Quantity: 101"
+        )
+
+        app = Application.new(
+          database = "shop_manager_test",
+          io = fake_io,
+          fake_item_repo,
+          fake_order_repo
+        )
+        app.run
+      end
     end
-
-    # it "gets an option from the user" do
-    #   fake_io = double :fake_io
-
-    #   expect(fake_io)
-    #   app = Application.new(fake_io)
-    #   app.run
-    # end
   end
 end
