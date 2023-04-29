0dc20730ad7ef7477929b1755816784c6de2fba0
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-29 15:31:22 +0100

    refactor

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -2,19 +2,29 @@ require_relative 'database_connection'
 require_relative 'item'
 
 class ItemRepository
+  def initialize
+    @items = []
+  end
+
   def all
     sql = "SELECT id, name, unit_price, stock_quantity FROM items;"
     result_set = DatabaseConnection.exec_params(sql, [])
     
-    items = []
     result_set.each do |record|
-      item = Item.new
-      item.id = record["id"]
-      item.name = record["name"]
-      item.unit_price = record["unit_price"]
-      item.stock_quantity = record['stock_quantity']
-      items << item
+      @items << single_item(record)
     end
-    return items
+    return @items
   end
+
+  private
+
+  def single_item(record)
+    item = Item.new
+    item.id = record["id"]
+    item.name = record["name"]
+    item.unit_price = record["unit_price"]
+    item.stock_quantity = record['stock_quantity']
+    return item
+  end
+
 end
