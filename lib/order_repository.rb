aa8744f00a97557f506fb3d7106157cb0c5f59d1
awdem <wdemontmollin@gmail.com>
2023-04-29 11:07:12 +0100

    implemented OrderRepository#all"

--- lib/order_repository.rb
+++ lib/order_repository.rb
@@ -3,12 +3,18 @@ require_relative 'order'
 class OrderRepository
 
   # Selecting all records
-  # No arguments
   def all
-    # Executes the SQL query:
-    # SELECT * FROM orders;
+    orders = []
+    sql = 'SELECT * FROM orders;'
+    result_set = DatabaseConnection.exec_params(sql, [])
 
-    # Returns an array of order objects.
+    result_set.each do |record|
+      order = Order.new
+      set_attributes(order, record)
+      orders << order
+    end
+
+    orders
   end
 
   # creates a new order
@@ -26,4 +32,12 @@ class OrderRepository
     # returns nothing.
   end
 
+
+  private
+
+  def set_attributes(order, record)
+    order.id = record['id'].to_i
+    order.customer_name = record['customer_name']
+    order.order_date = record['order_date']
+  end
 end
