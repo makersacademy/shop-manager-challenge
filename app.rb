962c83eb98afd1ee9884cbe4cda4e9dcb3053dd9
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-29 15:50:53 +0100

    add app.rb and design

---
+++ app.rb
@@ -0,0 +1,42 @@
+require_relative './lib/item_repository'
+require_relative './lib/order_repository'
+
+class Application
+
+  # The Application class initializer
+  # takes four arguments:
+  #  * The database name to call `DatabaseConnection.connect`
+  #  * the Kernel object as `io` (so we can mock the IO in our tests)
+  #  * the AlbumRepository object (or a double of it)
+  #  * the ArtistRepository object (or a double of it)
+  def initialize(database_name, io, item_repository, order_repository)
+    DatabaseConnection.connect(database_name)
+    @io = io
+    @item_repository = item_repository
+    @order_repository = order_repository
+  end
+
+  def run
+    # "Runs" the terminal application
+    # so it can ask the user to enter some input
+    # and then decide to run the appropriate action
+    # or behaviour.
+
+    # Use `@io.puts` or `@io.gets` to
+    # write output and ask for user input.
+  end
+end
+
+# Don't worry too much about this if statement. It is basically saying "only
+# run the following code if this is the main file being run, instead of having
+# been required or loaded by another file.
+# If you want to learn more about __FILE__ and $0, see here: https://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Variables_and_Constants#Pre-defined_Variables
+if __FILE__ == $0
+  app = Application.new(
+    'music_library',
+    Kernel,
+    ItemRepository.new,
+    OrderRepository.new
+  )
+  app.run
+end
