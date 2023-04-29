16aa12e748c575b2e05d034787332b9bd2a61098
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-04-29 09:38:47 +0100

    app.rb file updated

--- app.rb
+++ app.rb
@@ -2,18 +2,27 @@ require_relative 'lib/database_connection'
 require_relative 'lib/item_repository'
 require_relative 'lib/order_repository'
 
+
 class Application
-  def initialize(database_name, io, item_repository, order_repository)
+  def initialize(database_name, io, item_repository, order_repository, item_class, order_class)
     DatabaseConnection.connect(database_name)
     @io = io
     @item_repository = item_repository
     @order_repository = order_repository
+    @item_class = item_class
+    @order_class = order_class
   end
 
   def run
     @io.puts "Welcome to the shop management program!"
-
-    get_user_choice
+    @io.puts ""
+    choice = get_user_choice
+    case choice
+    when "1"
+      display_items
+    when "2"
+      create_item
+    end
   end
 
   private
@@ -24,13 +33,34 @@ class Application
     @io.puts "  2 = create a new item"
     @io.puts "  3 = list all orders"
     @io.puts "  4 = create a new order"
+    choice = @io.gets.chomp
+    @io.puts ""
+    return choice
+  end
+
+  def display_items
+    @io.puts "Here's a list of all shop items:"
+    @io.puts ""
+    @item_repository.all.each do |item|
+      @io.puts "##{item.id} #{item.name} - Unit price: #{item.unit_price} - Quantity: #{item.quantity}"
+    end
+  end
+
+  def create_item
+    item = @item_class.new
+    @io.puts "Item name:"
+    item.name = @io.gets.chomp
+    @io.puts "Item price:"
+    item.unit_price = @io.gets.chomp.to_f
+    @io.puts "Item quantity:"
+    item.quantity = @io.gets.chomp.to_i
+    @item_repository.create(item)
   end
 end
 
-
 if __FILE__ == $0
   app = Application.new(
-    'shop_manager',
+    'shop_manager_test',
     Kernel,
     ItemRepository.new,
     OrderRepository.new
---
+++ spec/app_spec.rb
@@ -0,0 +1,72 @@
+require_relative '../app'
+
+def test_introduction(io)
+  expect(io).to receive(:puts).with "Welcome to the shop management program!"
+  expect(io).to receive(:puts).with ""
+  expect(io).to receive(:puts).with "What do you want to do?"
+  expect(io).to receive(:puts).with "  1 = list all shop items"
+  expect(io).to receive(:puts).with "  2 = create a new item"
+  expect(io).to receive(:puts).with "  3 = list all orders"
+  expect(io).to receive(:puts).with "  4 = create a new order"
+end
+
+RSpec.describe Application do
+  it 'prints all the items' do
+    item1 = double(:item, id: 1, name: "Balloon", unit_price: 2.99, quantity: 5)
+    item2 = double(:item, id: 2, name: "Animal", unit_price: 3, quantity: 1)
+    item_repo = double(:item_repository, all: [item1, item2])
+    order_repo = double(:order_repository)
+    item_class = double(:item_class)
+    order_class = double(:order_class)
+
+    io = double(:io)
+    test_introduction(io)
+
+    expect(io).to receive(:gets).and_return "1"
+    expect(io).to receive(:puts).with ""
+    expect(io).to receive(:puts).with "Here's a list of all shop items:"
+    expect(io).to receive(:puts).with ""
+    expect(io).to receive(:puts).with "#1 Balloon - Unit price: 2.99 - Quantity: 5"
+    expect(io).to receive(:puts).with "#2 Animal - Unit price: 3 - Quantity: 1"
+    
+    app = Application.new(
+      'shop_manager_test', io, 
+      item_repo, order_repo, 
+      item_class, order_class
+    )
+    app.run
+  end
+
+  it 'creates a new item' do
+    order_repo = double(:order_repository)
+    item_repo = double(:item_repository)
+    item = double(:item)
+    expect(item).to receive(:name=).with("Chocolate")
+    expect(item).to receive(:unit_price=).with(1.99)
+    expect(item).to receive(:quantity=).with(3)
+
+    item_class = double(:item_class, new: item)
+    order_class = double(:order_class)
+
+    io = double(:io)
+    test_introduction(io)
+
+    expect(io).to receive(:gets).and_return "2"
+    expect(io).to receive(:puts).with ""
+    expect(io).to receive(:puts).with "Item name:"
+    expect(io).to receive(:gets).and_return "Chocolate"
+    expect(io).to receive(:puts).with "Item price:"
+    expect(io).to receive(:gets).and_return "1.99"
+    expect(io).to receive(:puts).with "Item quantity:"
+    expect(io).to receive(:gets).and_return "3"
+
+    expect(item_repo).to receive(:create).with(item)
+
+    app = Application.new(
+      'shop_manager_test', io, 
+      item_repo, order_repo, 
+      item_class, order_class
+    )
+    app.run
+  end
+end
