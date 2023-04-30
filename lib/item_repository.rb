0e063c5a17c1a912143816ba0efaa4b2bae89e28
Evan Thomas <evanthomas0292@gmail.com>
2023-04-30 10:56:28 +0100

    Add ItemRepository#find

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -14,6 +14,12 @@ class ItemRepository
     params = [item.name, item.unit_price, item.quantity]
     DatabaseConnection.exec_params(sql, params)
   end
+
+  def find(id)
+    sql = "SELECT * FROM items WHERE id = $1;"
+    records = DatabaseConnection.exec_params(sql, [id])
+    convert_record_to_item(records[0])
+  end
   
   private
   
