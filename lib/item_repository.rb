f55156880274dbb7d6cbf2cf20160f5b374512d0
Matt Hammond <mjhammond94@gmail.com>
2023-05-01 11:43:31 +0100

    spec test added for item repo - all method added - test passed

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -3,7 +3,16 @@ require 'item'
 class ItemRepository
 
   def all
-    
+    result = DatabaseConnection.exec_params('SELECT * FROM items;', [])
+    all_items = []
+    result.each do |row| 
+      item = Item.new
+      item.id = row['id']
+      item.item_name = row['item_name']
+      item.unit_price = row['unit_price']
+      item.quantity = row['quantity']
+      all_items << item
+    end
+    all_items
   end
-
 end
--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -1,2 +1,23 @@
 require 'item_repository'
 
+describe ItemRepository do
+
+  def reset_items_table
+    seed_sql = File.read('spec/item_seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_manager_test' })
+    connection.exec(seed_sql)
+  end
+
+  before(:each) do
+    reset_items_table
+  end
+
+  it 'Should return all rows as an array when the all method is called' do
+    repo = ItemRepository.new
+    result = repo.all
+    expect(result.length).to eq 3
+    expect(result.first.id).to eq '1'
+    expect(result.last.id).to eq '3'
+    expect(result[1].item_name).to eq 'Lead-Pipe'
+  end
+end
