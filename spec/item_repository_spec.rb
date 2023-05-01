679b89fb2203f0837291209dad179b767a6672ab
Matt Hammond <mjhammond94@gmail.com>
2023-05-01 11:48:08 +0100

    test added to item repo spec - test passed

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -1,7 +1,6 @@
 require 'item_repository'
 
 describe ItemRepository do
-
   def reset_items_table
     seed_sql = File.read('spec/item_seeds.sql')
     connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_manager_test' })
@@ -19,5 +18,24 @@ describe ItemRepository do
     expect(result.first.id).to eq '1'
     expect(result.last.id).to eq '3'
     expect(result[1].item_name).to eq 'Lead-Pipe'
+  end
+end
+
+describe ItemRepository do
+  context 'Empty databse' do
+    def reset_items_table
+      seed_sql = File.read('spec/empty_seeds.sql')
+      connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_manager_test' })
+      connection.exec(seed_sql)
+    end
+
+    before(:each) do
+      reset_items_table
+    end
+
+    it 'Should return all rows as an array when the all method is called' do
+      repo = ItemRepository.new
+      expect(repo.all).to eq []
+    end
   end
 end
