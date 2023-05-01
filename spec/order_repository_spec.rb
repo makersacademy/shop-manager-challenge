cae445c2bf3be69fde0f1bc1d88089ce5a8c0765
Matt Hammond <mjhammond94@gmail.com>
2023-05-01 11:55:39 +0100

    Spec tests added for order repo - RED

--- spec/order_repository_spec.rb
+++ spec/order_repository_spec.rb
@@ -1,1 +1,23 @@
 require 'order_repository'
+
+describe OrderRepository do
+  def reset_orders_table
+    seed_sql = File.read('spec/order_seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_manager_test' })
+    connection.exec(seed_sql)
+  end
+
+  before(:each) do
+    reset_orders_table
+  end
+
+  it 'Should return all rows as an array of order objects when the all method is called' do
+    repo = OrderRepository.new
+    result = repo.all
+    expect(result.length).to eq 9
+    expect(result.first.id).to eq '1'
+    expect(result.last.id).to eq '9'
+    expect(result[4].customer_name).to eq 'Mrs Peacock'
+    expect(result[5].item_id).to eq '1'
+  end
+end
