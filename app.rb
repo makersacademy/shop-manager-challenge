d56ca4fa9846effe78c8574c7fac9528ba64f5fb
Matt Hammond <mjhammond94@gmail.com>
2023-05-01 14:37:29 +0100

    test added - app updated - test passed

--- app.rb
+++ app.rb
@@ -1,19 +1,30 @@
 require_relative 'lib/database_connection'
-
+require_relative 'lib/item_repository'
+require_relative 'lib/order_repository'
 
 class Application 
 
-  def initialize(io)
+  def initialize(database_name, io, item_repository, order_repository)
+    DatabaseConnection.connect(database_name)
     @io = io
+    @item_repository = item_repository
+    @order_repository = order_repository
   end
 
   def run
     @io.puts "What do you want to do?"
     @io.puts "1 = list all shop items\n2 = create a new item\n3 = list all orders\n4 = create a new order"
     selection = @io.gets.chomp
+    if selection == "1"
+      @item_repository.all.each { |item| @io.puts "##{item.id} - #{item.item_name} - Unit price: #{item.unit_price} - Quantity: #{item.quantity}" }
+    else nil
+    end
   end
 end
 
-io = Kernel
-app = Application.new(io)
-app.run
+# name = 'shop_manager'
+# io = Kernel
+# item_repo = ItemRepository.new
+# order_repo = OrderRepository.new
+# app = Application.new(name, io, item_repo, order_repo)
+# app.run
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,11 +1,28 @@
 require_relative '../app'
 
 describe Application do 
-  it 'Should return a list of options to the user' do
+
+  def reset_items_table
+    seed_sql = File.read('spec/item_seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_manager_test' })
+    connection.exec(seed_sql)
+  end
+
+  before(:each) do
+    reset_items_table
+  end
+
+  it 'Should return all shop items in a list when 1 is selected by user' do
     io = double :io
-    expect(io).to receive(:puts).with("What do you want to do?")
-    expect(io).to receive(:puts).with("1 = list all shop items\n2 = create a new item\n3 = list all orders\n4 = create a new order")
-    app = Application.new(io)
+    order_repo = double :order
+    item_repo = ItemRepository.new
+    expect(io).to receive(:puts).with("What do you want to do?").ordered
+    expect(io).to receive(:puts).with("1 = list all shop items\n2 = create a new item\n3 = list all orders\n4 = create a new order").ordered
+    expect(io).to receive(:gets).and_return("1")
+    expect(io).to receive(:puts).with("#1 - Candlestick - Unit price: 1.99 - Quantity: 10")
+    expect(io).to receive(:puts).with("#2 - Lead-Pipe - Unit price: 4.45 - Quantity: 3")
+    expect(io).to receive(:puts).with("#3 - Gun - Unit price: 12.95 - Quantity: 1")
+    app = Application.new('shop_manager_test', io, item_repo, order_repo)
     app.run
   end
 end
