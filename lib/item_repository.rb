87e17d2f9aa787872b1a5b3b82c7ab3993cef635
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-05-27 19:26:54 +0100

    add .all, .find, .create methods(items repo) and tests for them, passes tests

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,17 +1,56 @@
-class ItemRepository
+require_relative './item'
 
+class ItemRepository
   # Selecting all items
   # No arguments
   def all
-    # Executes the SQL query:
-    # SELECT id, name, unit_price, quantity FROM items;
-    # Returns an array of Item objects.
+    items = []
+    #defining sql query string
+    sql = 'SELECT id, name, unit_price, quantity FROM items;'
+    #executes sql query and passes sql string and an empty array as an argument
+    result_set = DatabaseConnection.exec_params(sql, [])
+
+    result_set.each do |inst|
+      #for each instance it creates a new instance and assigns corresponding atributes
+      item = Item.new
+
+      item.id = inst['id']
+      item.name = inst['name']
+      item.unit_price = inst['unit_price']
+      item.quantity = inst['quantity']
+
+      items << item
+    end
+
+    return items
   end
 
   def find(id)
-    # id is an integer representing the item ID to search for
-    # SELECT name, unit_price, quantity FROM items WHERE id = $1;
-    # Returns an instance of Item object
+    items = []
+    sql = 'SELECT name, unit_price, quantity FROM items WHERE id = $1;'
+    params = [id]
+    result = DatabaseConnection.exec_params(sql, params)
+
+    result.each do |inst|
+      item = Item.new
+
+      item.id = inst['id']
+      item.name = inst['name']
+      item.unit_price = inst['unit_price']
+      item.quantity = inst['quantity']
+
+      return item
+    end
   end
 
+  def create(item)
+    
+    sql = 'INSERT INTO items (name, unit_price, quantity) VALUES ($1, $2, $3);'
+    params = [item.name, item.unit_price, item.quantity]
+
+    DatabaseConnection.exec_params(sql, params)
+
+    return nil
+
+  end
 end
--- spec/items_spec.rb
+++ spec/items_spec.rb
@@ -2,16 +2,50 @@ require 'item_repository'
 
 
 RSpec.describe ItemRepository do
-def reset_shop_manager_table
-  seed_sql = File.read('spec/seeds_shop_manager.sql')
-  connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_manager_test' })
-  connection.exec(seed_sql)
-end
+  def reset_shop_manager_table
+    seed_sql = File.read('spec/seeds_shop_manager.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_manager_test' })
+    connection.exec(seed_sql)
+  end
 
 
-before(:each) do 
-  reset_shop_manager_table
-end
+  before(:each) do 
+    reset_shop_manager_table
+  end
 
-  # (your tests will go here).
+  it "returns two items" do
+    repo = ItemRepository.new
+
+    items = repo.all
+    expect(items.length).to eq 2 # => 2
+    expect(items.first.name).to eq "CPU" # => 'CPU'
+    expect(items.first.unit_price).to eq '199.99' # => '199.99'
+    expect(items.first.quantity).to eq '10' # => '10'
+  end
+
+  it 'returns the item with the specified ID' do
+    repo = ItemRepository.new
+    item = repo.find(2)
+
+    #expect(album.first.id).to eq(1)
+    expect(item.name).to eq('GPU')
+    expect(item.unit_price).to eq('499.99')
+    expect(item.quantity).to eq("5")
+  end
+
+  it "creates a new instance in the database and returns it" do
+    repo = ItemRepository.new
+
+    new_item = Item.new
+    new_item.name = 'Keyboard'
+    new_item.unit_price = 59.99
+    new_item.quantity = 8
+    
+    repo.create(new_item)
+    
+    all_items = repo.all
+    expect(all_items.last.name).to eq('Keyboard') 
+    expect(all_items.last.unit_price).to eq('59.99')
+    expect(all_items.last.quantity).to eq('8')
+  end
 end
