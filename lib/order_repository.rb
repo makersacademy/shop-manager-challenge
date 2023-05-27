0c1351a1c54f0063dd52f5b465a854878a4b7a52
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-05-27 19:44:45 +0100

    add .all method and test to the order repo, passes test

--- lib/order_repository.rb
+++ lib/order_repository.rb
@@ -1,11 +1,28 @@
+require_relative './order'
+
 class OrderRepository
 
   # Selecting all orders
   # No arguments
   def all
-    # Executes the SQL query:
-    # SELECT id, customer_name, order_date FROM orders;
-    # Returns an array of Order objects.
+    orders = []
+    #defining sql query string
+    sql = 'SELECT id, customer_name, order_date FROM orders;'
+    #executes sql query and passes sql string and an empty array as an argument
+    result_set = DatabaseConnection.exec_params(sql, [])
+
+    result_set.each do |inst|
+      #for each instance it creates a new instance and assigns corresponding atributes
+      order = Order.new
+
+      order.id = inst['id']
+      order.customer_name = inst['customer_name']
+      order.order_date = inst['order_date']
+
+      orders << order
+    end
+
+    return orders
   end
 
   def find(id)
---
+++ spec/orders_spec.rb
@@ -0,0 +1,25 @@
+require 'order_repository'
+
+
+RSpec.describe OrderRepository do
+  def reset_shop_manager_table
+    seed_sql = File.read('spec/seeds_shop_manager.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'shop_manager_test' })
+    connection.exec(seed_sql)
+  end
+
+
+  before(:each) do 
+    reset_shop_manager_table
+  end
+
+  it "returns two orders" do
+    repo = OrderRepository.new
+
+    orders = repo.all
+    expect(orders.length).to eq 2 # => 2
+    expect(orders.first.customer_name).to eq "Joe Hannis" 
+    expect(orders.first.order_date).to eq '2023-05-25' 
+  end
+
+end
